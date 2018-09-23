61e6c4bd3d27b86cf5db3f8f54edf4cc0e510531
darksideofg <42369239+darksideofg@users.noreply.github.com>
2018-09-23 05:20:48 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 25509,
-	"created_at": "Sat Sep 22 03:20:47 +0000 2018",
+	"tweets": 25510,
+	"created_at": "Sun Sep 23 03:20:48 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_09.js
+++ data/js/tweets/2018_09.js
@@ -1,6 +1,106 @@
 Grailbird.data.tweets_2018_09 = 
 [
 	{
+		"created_at": "Sat Sep 22 12:29:12 +0000 2018",
+		"id": 1043477310710722600,
+		"id_str": "1043477310710722560",
+		"text": "@DukeOfSuffolk LastPass o 1Password.",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DukeOfSuffolk",
+					"name": "Sailor Stupider",
+					"id": 43511524,
+					"id_str": "43511524",
+					"indices": [
+						0,
+						14
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1043476191766818800,
+		"in_reply_to_status_id_str": "1043476191766818816",
+		"in_reply_to_user_id": 43511524,
+		"in_reply_to_user_id_str": "43511524",
+		"in_reply_to_screen_name": "DukeOfSuffolk",
+		"user": {
+			"id": 535277787,
+			"id_str": "535277787",
+			"name": "G (̄・・̄)",
+			"screen_name": "DarkSideOfG",
+			"location": "εᴍ - Siberia",
+			"description": "“The drought was the very worst / When the flowers that we'd grown together died of thirst”",
+			"url": "https://t.co/kzM9iE3UtX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kzM9iE3UtX",
+							"expanded_url": "http://darksideofg.tk",
+							"display_url": "darksideofg.tk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 119,
+			"friends_count": 74,
+			"listed_count": 10,
+			"created_at": "Sat Mar 24 09:46:29 +0000 2012",
+			"favourites_count": 7727,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 25466,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "515151",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_link_color": "686868",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "595959",
+			"profile_text_color": "606060",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Fri Sep 21 16:32:39 +0000 2018",
 		"id": 1043176188699705300,
 		"id_str": "1043176188699705345",
@@ -142,7 +242,7 @@ Grailbird.data.tweets_2018_09 =
 							"name": "crazy broke asian",
 							"screen_name": "tribranchvo",
 							"location": "the bad place",
-							"description": "graphic designer. LGBT. Beyhive 👩‍🎨🌈🐝 // formerly @mic, @cerosdotcom // currently @considerablehq",
+							"description": "graphic designer. LGBT. Beyhive 👩🎨🌈🐝 // formerly @mic, @cerosdotcom // currently @considerablehq",
 							"url": "https://t.co/4ctwC8HAuy",
 							"entities": {
 								"url": {
@@ -404,7 +504,7 @@ Grailbird.data.tweets_2018_09 =
 				"name": "crazy broke asian",
 				"screen_name": "tribranchvo",
 				"location": "the bad place",
-				"description": "graphic designer. LGBT. Beyhive 👩‍🎨🌈🐝 // formerly @mic, @cerosdotcom // currently @considerablehq",
+				"description": "graphic designer. LGBT. Beyhive 👩🎨🌈🐝 // formerly @mic, @cerosdotcom // currently @considerablehq",
 				"url": "https://t.co/4ctwC8HAuy",
 				"entities": {
 					"url": {
