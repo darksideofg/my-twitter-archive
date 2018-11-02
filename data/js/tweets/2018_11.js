88dea1a3aa3ecc145b850e2fe9730c36f00daae4
darksideofg <42369239+darksideofg@users.noreply.github.com>
2018-11-02 04:20:48 +0100

    Added 1 tweets

---
+++ data/js/tweets/2018_11.js
@@ -0,0 +1,177 @@
+Grailbird.data.tweets_2018_11 = 
+[
+	{
+		"created_at": "Tue Oct 16 20:02:23 +0000 2018",
+		"id": 1052288669061591000,
+		"id_str": "1052288669061591040",
+		"full_text": "I’m screaming https://t.co/GkuludySW6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			13
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1052288665211129900,
+					"id_str": "1052288665211129856",
+					"indices": [
+						14,
+						37
+					],
+					"media_url": "http://pbs.twimg.com/media/Dpp69zsUUAAy1eC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dpp69zsUUAAy1eC.jpg",
+					"url": "https://t.co/GkuludySW6",
+					"display_url": "pic.twitter.com/GkuludySW6",
+					"expanded_url": "https://twitter.com/SigmaRue/status/1052288669061591040/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 748,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 424,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 748,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1052288665211129900,
+					"id_str": "1052288665211129856",
+					"indices": [
+						14,
+						37
+					],
+					"media_url": "http://pbs.twimg.com/media/Dpp69zsUUAAy1eC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dpp69zsUUAAy1eC.jpg",
+					"url": "https://t.co/GkuludySW6",
+					"display_url": "pic.twitter.com/GkuludySW6",
+					"expanded_url": "https://twitter.com/SigmaRue/status/1052288669061591040/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 748,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 424,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 748,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 23559552,
+			"id_str": "23559552",
+			"name": "Stephanie 🔜 PDXLAN",
+			"screen_name": "SigmaRue",
+			"location": "Portland, OR",
+			"description": "#Twitch Affiliate | Web QA | Cosplayer | MMO addict | Writer | Outdoorsy person | #FFXIV Balmung | @lootcaveco Brand Ambassador | Avi by @Jazzy_lea",
+			"url": "https://t.co/iBolzy66M4",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/iBolzy66M4",
+							"expanded_url": "http://twitch.tv/sigmarue",
+							"display_url": "twitch.tv/sigmarue",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2825,
+			"friends_count": 802,
+			"listed_count": 151,
+			"created_at": "Tue Mar 10 05:09:10 +0000 2009",
+			"favourites_count": 14385,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 46474,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1058063783954808832/Tpg1si3K_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1058063783954808832/Tpg1si3K_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/23559552/1540581998",
+			"profile_link_color": "9266CC",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "2D0047",
+			"profile_text_color": "FFFFFF",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12170,
+		"favorite_count": 25776,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I’m screaming https://t.co/GkuludySW6"
+	}
+]
