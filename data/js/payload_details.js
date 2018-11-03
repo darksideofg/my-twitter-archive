78d7de9c0fbcae908b26b0460e0b7d769fc95486
darksideofg <42369239+darksideofg@users.noreply.github.com>
2018-11-03 04:20:48 +0100

    Added 9 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 25549,
-	"created_at": "Fri Nov 02 03:20:47 +0000 2018",
+	"tweets": 25558,
+	"created_at": "Sat Nov 03 03:20:47 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,484 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Thu Nov 01 00:41:24 +0000 2018",
+		"id": 1057794700109926400,
+		"id_str": "1057794700109926400",
+		"full_text": "God releasing his Fall animal collection now. https://t.co/P2MhTsJspv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			45
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/P2MhTsJspv",
+					"expanded_url": "https://twitter.com/MatthewACherry/status/1057679272666525696",
+					"display_url": "twitter.com/MatthewACherry…",
+					"indices": [
+						46,
+						69
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 517630091,
+			"id_str": "517630091",
+			"name": "KlayMani",
+			"screen_name": "SaintBishopK",
+			"location": "",
+			"description": "nahh dawg",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1545,
+			"friends_count": 339,
+			"listed_count": 13,
+			"created_at": "Wed Mar 07 14:19:51 +0000 2012",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 74395,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1049516034921811968/xZ4aY6fe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1049516034921811968/xZ4aY6fe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/517630091/1537067452",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1057679272666525700,
+		"quoted_status_id_str": "1057679272666525696",
+		"quoted_status_permalink": {
+			"url": "https://t.co/P2MhTsJspv",
+			"expanded": "https://twitter.com/MatthewACherry/status/1057679272666525696",
+			"display": "twitter.com/MatthewACherry…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Oct 31 17:02:43 +0000 2018",
+			"id": 1057679272666525700,
+			"id_str": "1057679272666525696",
+			"full_text": "Here's a video of one. If this ain't the flyest duck you ever saw https://t.co/tyhfAeawez",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				65
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1057679099462803500,
+						"id_str": "1057679099462803456",
+						"indices": [
+							66,
+							89
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1057679099462803456/pu/img/FA_DxH51Gd6JZCDh.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1057679099462803456/pu/img/FA_DxH51Gd6JZCDh.jpg",
+						"url": "https://t.co/tyhfAeawez",
+						"display_url": "pic.twitter.com/tyhfAeawez",
+						"expanded_url": "https://twitter.com/MatthewACherry/status/1057679272666525696/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1057679099462803500,
+						"id_str": "1057679099462803456",
+						"indices": [
+							66,
+							89
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1057679099462803456/pu/img/FA_DxH51Gd6JZCDh.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1057679099462803456/pu/img/FA_DxH51Gd6JZCDh.jpg",
+						"url": "https://t.co/tyhfAeawez",
+						"display_url": "pic.twitter.com/tyhfAeawez",
+						"expanded_url": "https://twitter.com/MatthewACherry/status/1057679272666525696/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 140000,
+							"variants": [
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1057679099462803456/pu/vid/1280x720/9KMaEfVdRoZC4DHw.mp4?tag=5"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1057679099462803456/pu/pl/x6HoNM8Es02KSwBI.m3u8?tag=5"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1057679099462803456/pu/vid/640x360/AQF9_RPDu9EXozIC.mp4?tag=5"
+								},
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1057679099462803456/pu/vid/320x180/Fh6Hgf9dCmpFgRX9.mp4?tag=5"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": 1057674858409017300,
+			"in_reply_to_status_id_str": "1057674858409017344",
+			"in_reply_to_user_id": 27384718,
+			"in_reply_to_user_id_str": "27384718",
+			"in_reply_to_screen_name": "MatthewACherry",
+			"user": {
+				"id": 27384718,
+				"id_str": "27384718",
+				"name": "Matthew A. Cherry",
+				"screen_name": "MatthewACherry",
+				"location": "Chicago/LA",
+				"description": "Director | NFL Alum | Gif Connoisseur | Director Credits: The Last Fall, 9 Rides, Hair Love, The Last O.G., The Red Line, Whiskey Cavalier",
+				"url": "https://t.co/ZS1UnAGq5J",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/ZS1UnAGq5J",
+								"expanded_url": "http://www.matthewacherry.com/",
+								"display_url": "matthewacherry.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 112455,
+				"friends_count": 9817,
+				"listed_count": 2621,
+				"created_at": "Sun Mar 29 05:09:42 +0000 2009",
+				"favourites_count": 81088,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 162529,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/859464069912424448/xJA69oH6_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/859464069912424448/xJA69oH6_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/27384718/1528386016",
+				"profile_link_color": "57A8FF",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "F0F0F0",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 5575,
+			"favorite_count": 23536,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 56797,
+		"favorite_count": 214901,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "God releasing his Fall animal collection now. https://t.co/P2MhTsJspv"
+	},
+	{
+		"created_at": "Tue Oct 16 20:02:23 +0000 2018",
+		"id": 1052288669061591000,
+		"id_str": "1052288669061591040",
+		"full_text": "I’m screaming https://t.co/GkuludySW6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			13
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1052288665211129900,
+					"id_str": "1052288665211129856",
+					"indices": [
+						14,
+						37
+					],
+					"media_url": "http://pbs.twimg.com/media/Dpp69zsUUAAy1eC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dpp69zsUUAAy1eC.jpg",
+					"url": "https://t.co/GkuludySW6",
+					"display_url": "pic.twitter.com/GkuludySW6",
+					"expanded_url": "https://twitter.com/SigmaRue/status/1052288669061591040/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 748,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 424,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 748,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1052288665211129900,
+					"id_str": "1052288665211129856",
+					"indices": [
+						14,
+						37
+					],
+					"media_url": "http://pbs.twimg.com/media/Dpp69zsUUAAy1eC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dpp69zsUUAAy1eC.jpg",
+					"url": "https://t.co/GkuludySW6",
+					"display_url": "pic.twitter.com/GkuludySW6",
+					"expanded_url": "https://twitter.com/SigmaRue/status/1052288669061591040/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 748,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 424,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 748,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 23559552,
+			"id_str": "23559552",
+			"name": "Stephanie 🔜 PDXLAN",
+			"screen_name": "SigmaRue",
+			"location": "Portland, OR",
+			"description": "#Twitch Affiliate | Web QA | Cosplayer | MMO addict | Writer | Outdoorsy person | #FFXIV Balmung | @lootcaveco Brand Ambassador | Avi by @Jazzy_lea",
+			"url": "https://t.co/iBolzy66M4",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/iBolzy66M4",
+							"expanded_url": "http://twitch.tv/sigmarue",
+							"display_url": "twitch.tv/sigmarue",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2830,
+			"friends_count": 802,
+			"listed_count": 152,
+			"created_at": "Tue Mar 10 05:09:10 +0000 2009",
+			"favourites_count": 14394,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 46484,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1058063783954808832/Tpg1si3K_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1058063783954808832/Tpg1si3K_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/23559552/1540581998",
+			"profile_link_color": "9266CC",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "2D0047",
+			"profile_text_color": "FFFFFF",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12201,
+		"favorite_count": 25869,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I’m screaming https://t.co/GkuludySW6"
+	},
+	{
 		"created_at": "Tue Oct 16 20:02:23 +0000 2018",
 		"id": 1052288669061591000,
 		"id_str": "1052288669061591040",
