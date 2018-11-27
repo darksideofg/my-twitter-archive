e0d0f1f8c2a1cfdc44ecbd8e483dc10bc9bea513
darksideofg <42369239+darksideofg@users.noreply.github.com>
2018-11-27 04:20:48 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 25600,
-	"created_at": "Sun Nov 25 03:20:47 +0000 2018",
+	"tweets": 25601,
+	"created_at": "Tue Nov 27 03:20:47 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Sun Nov 25 00:15:13 +0000 2018",
+		"id": 1066485421515788300,
+		"id_str": "1066485421515788289",
+		"full_text": "i just read something like 'hey you know how long 2018 has been? we had an olympics this year and everybody forgot about it.' \n\nand i just stared off into the distance like",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			172
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1391657142,
+			"id_str": "1391657142",
+			"name": "Premee",
+			"screen_name": "premeesaurus",
+			"location": "Edmonton, Alberta",
+			"description": "SFF writer, scientist, beetle. Debut novel BENEATH THE RISING out in 2020 (Solaris Books). Short fiction in a variety o' venues! Repped by @mkcurry. She/her",
+			"url": "https://t.co/CtDHgbHLGV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/CtDHgbHLGV",
+							"expanded_url": "http://www.premeemohamed.com",
+							"display_url": "premeemohamed.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4075,
+			"friends_count": 4979,
+			"listed_count": 86,
+			"created_at": "Tue Apr 30 09:56:21 +0000 2013",
+			"favourites_count": 69032,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 42537,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "B2DFDA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/709248805011492871/01dgKkaG_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/709248805011492871/01dgKkaG_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1391657142/1423821597",
+			"profile_link_color": "93A644",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 76113,
+		"favorite_count": 341581,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "i just read something like 'hey you know how long 2018 has been? we had an olympics this year and everybody forgot about it.' \n\nand i just stared off into the distance like"
+	},
+	{
 		"created_at": "Sat Nov 24 18:45:22 +0000 2018",
 		"id": 1066402413626761200,
 		"id_str": "1066402413626761217",
