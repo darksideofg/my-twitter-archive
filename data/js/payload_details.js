c60fde9ba68d9e008edfef4f7f8e05849d1a46a5
darksideofg <42369239+darksideofg@users.noreply.github.com>
2018-12-03 04:20:49 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 25609,
-	"created_at": "Sun Dec 02 03:20:48 +0000 2018",
+	"tweets": 25611,
+	"created_at": "Mon Dec 03 03:20:48 +0000 2018",
 	"lang": "en"
 }
---
+++ data/js/tweets/2018_12.js
@@ -0,0 +1,371 @@
+Grailbird.data.tweets_2018_12 = 
+[
+	{
+		"created_at": "Sun Dec 02 22:04:37 +0000 2018",
+		"id": 1069351658046595100,
+		"id_str": "1069351658046595073",
+		"full_text": "Is this @fedebruni84 https://t.co/X3bc0FM4lM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			20
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "fedebruni84",
+					"name": "Federico 🐍",
+					"id": 23112137,
+					"id_str": "23112137",
+					"indices": [
+						8,
+						20
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/X3bc0FM4lM",
+					"expanded_url": "https://twitter.com/aly_stahp/status/1067055473118011394",
+					"display_url": "twitter.com/aly_stahp/stat…",
+					"indices": [
+						21,
+						44
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 535277787,
+			"id_str": "535277787",
+			"name": "G (̄・・̄)",
+			"screen_name": "DarkSideOfG",
+			"location": "εᴍ - Siberia",
+			"description": "“The drought was the very worst / When the flowers that we'd grown together died of thirst”",
+			"url": "https://t.co/kzM9iE3UtX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kzM9iE3UtX",
+							"expanded_url": "http://darksideofg.tk",
+							"display_url": "darksideofg.tk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 116,
+			"friends_count": 74,
+			"listed_count": 10,
+			"created_at": "Sat Mar 24 09:46:29 +0000 2012",
+			"favourites_count": 7810,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 25534,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "515151",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_link_color": "686868",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "595959",
+			"profile_text_color": "606060",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1067055473118011400,
+		"quoted_status_id_str": "1067055473118011394",
+		"quoted_status_permalink": {
+			"url": "https://t.co/X3bc0FM4lM",
+			"expanded": "https://twitter.com/aly_stahp/status/1067055473118011394",
+			"display": "twitter.com/aly_stahp/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Nov 26 14:00:24 +0000 2018",
+			"id": 1067055473118011400,
+			"id_str": "1067055473118011394",
+			"full_text": "hi in case you’re having a bad day here are sum of my fave shots of bruni,, \n\nit’ll get better 🔆 https://t.co/8ABtqihN7R",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				96
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1067055432408027100,
+						"id_str": "1067055432408027136",
+						"indices": [
+							97,
+							120
+						],
+						"media_url": "http://pbs.twimg.com/media/Ds7xQyRU0AAx5oW.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Ds7xQyRU0AAx5oW.jpg",
+						"url": "https://t.co/8ABtqihN7R",
+						"display_url": "pic.twitter.com/8ABtqihN7R",
+						"expanded_url": "https://twitter.com/aly_stahp/status/1067055473118011394/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 900,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1536,
+								"h": 2048,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1067055432408027100,
+						"id_str": "1067055432408027136",
+						"indices": [
+							97,
+							120
+						],
+						"media_url": "http://pbs.twimg.com/media/Ds7xQyRU0AAx5oW.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Ds7xQyRU0AAx5oW.jpg",
+						"url": "https://t.co/8ABtqihN7R",
+						"display_url": "pic.twitter.com/8ABtqihN7R",
+						"expanded_url": "https://twitter.com/aly_stahp/status/1067055473118011394/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 900,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1536,
+								"h": 2048,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1067055432378724400,
+						"id_str": "1067055432378724352",
+						"indices": [
+							97,
+							120
+						],
+						"media_url": "http://pbs.twimg.com/media/Ds7xQyKVsAA9u-0.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Ds7xQyKVsAA9u-0.jpg",
+						"url": "https://t.co/8ABtqihN7R",
+						"display_url": "pic.twitter.com/8ABtqihN7R",
+						"expanded_url": "https://twitter.com/aly_stahp/status/1067055473118011394/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1067055432382873600,
+						"id_str": "1067055432382873600",
+						"indices": [
+							97,
+							120
+						],
+						"media_url": "http://pbs.twimg.com/media/Ds7xQyLVAAAcfKd.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Ds7xQyLVAAAcfKd.jpg",
+						"url": "https://t.co/8ABtqihN7R",
+						"display_url": "pic.twitter.com/8ABtqihN7R",
+						"expanded_url": "https://twitter.com/aly_stahp/status/1067055473118011394/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 852914744,
+				"id_str": "852914744",
+				"name": "aly",
+				"screen_name": "aly_stahp",
+				"location": "",
+				"description": "virgo sun & moon",
+				"url": "https://t.co/y2IJ48PNwc",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/y2IJ48PNwc",
+								"expanded_url": "http://instagram.com/illuminaly",
+								"display_url": "instagram.com/illuminaly",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 623,
+				"friends_count": 518,
+				"listed_count": 6,
+				"created_at": "Sat Sep 29 13:50:32 +0000 2012",
+				"favourites_count": 11308,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 29555,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1067746390447538177/_8kNrrca_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1067746390447538177/_8kNrrca_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/852914744/1543409689",
+				"profile_link_color": "4C4646",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 16928,
+			"favorite_count": 46915,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Is this @fedebruni84 https://t.co/X3bc0FM4lM"
+	}
+]
