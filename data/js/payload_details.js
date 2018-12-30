55cfe91b0479708c0f2ed3aab95f61c377501a0f
darksideofg <42369239+darksideofg@users.noreply.github.com>
2018-12-30 04:20:48 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 25634,
-	"created_at": "Sat Dec 29 03:20:47 +0000 2018",
+	"tweets": 25636,
+	"created_at": "Sun Dec 30 03:20:47 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -49,6 +49,274 @@ Grailbird.data.tweets_2018_12 =
 				}
 			},
 			"protected": false,
+			"followers_count": 4869,
+			"friends_count": 129,
+			"listed_count": 12,
+			"created_at": "Sat Dec 24 20:36:14 +0000 2011",
+			"favourites_count": 1590,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 568,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1057130730353053696/ViAoJORM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1057130730353053696/ViAoJORM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/445754523/1542557852",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4021,
+		"favorite_count": 24604,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "To all the boys I didn’t really like but then realized they liked me so I started liking them and then they stopped liking me so it made me like them more."
+	},
+	{
+		"created_at": "Tue Dec 25 17:10:36 +0000 2018",
+		"id": 1077612587364352000,
+		"id_str": "1077612587364352000",
+		"full_text": "Finalmente un regalo utile! https://t.co/89pKM5JGzD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			27
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1077612547975663600,
+					"id_str": "1077612547975663616",
+					"indices": [
+						28,
+						51
+					],
+					"media_url": "http://pbs.twimg.com/media/DvRy5yeX0AA1wCf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DvRy5yeX0AA1wCf.jpg",
+					"url": "https://t.co/89pKM5JGzD",
+					"display_url": "pic.twitter.com/89pKM5JGzD",
+					"expanded_url": "https://twitter.com/DarkSideOfG/status/1077612587364352000/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1077612547975663600,
+					"id_str": "1077612547975663616",
+					"indices": [
+						28,
+						51
+					],
+					"media_url": "http://pbs.twimg.com/media/DvRy5yeX0AA1wCf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DvRy5yeX0AA1wCf.jpg",
+					"url": "https://t.co/89pKM5JGzD",
+					"display_url": "pic.twitter.com/89pKM5JGzD",
+					"expanded_url": "https://twitter.com/DarkSideOfG/status/1077612587364352000/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 535277787,
+			"id_str": "535277787",
+			"name": "G (̄・・̄)",
+			"screen_name": "DarkSideOfG",
+			"location": "εᴍ - Siberia",
+			"description": "“The drought was the very worst / When the flowers that we'd grown together died of thirst”",
+			"url": "https://t.co/kzM9iE3UtX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kzM9iE3UtX",
+							"expanded_url": "http://darksideofg.tk",
+							"display_url": "darksideofg.tk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 117,
+			"friends_count": 74,
+			"listed_count": 10,
+			"created_at": "Sat Mar 24 09:46:29 +0000 2012",
+			"favourites_count": 7828,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 25539,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "515151",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_link_color": "686868",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "595959",
+			"profile_text_color": "606060",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "es",
+		"text": "Finalmente un regalo utile! https://t.co/89pKM5JGzD"
+	},
+	{
+		"created_at": "Sun Dec 23 23:30:34 +0000 2018",
+		"id": 1076983434864742400,
+		"id_str": "1076983434864742401",
+		"full_text": "To all the boys I didn’t really like but then realized they liked me so I started liking them and then they stopped liking me so it made me like them more.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			155
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 445754523,
+			"id_str": "445754523",
+			"name": "Hannah Berner",
+			"screen_name": "beingbernz",
+			"location": "Brooklyn, New York",
+			"description": "Podcast Host of Berning In Hell 🔥",
+			"url": "https://t.co/kzVPGmOagS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kzVPGmOagS",
+							"expanded_url": "https://itunes.apple.com/us/podcast/hannah-berner/id1442257788?mt=2",
+							"display_url": "itunes.apple.com/us/podcast/han…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
 			"followers_count": 4814,
 			"friends_count": 129,
 			"listed_count": 12,
