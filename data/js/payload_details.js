6b801b37fc4d902fd40c5bec140c19f932bf784c
darksideofg <42369239+darksideofg@users.noreply.github.com>
2019-01-07 04:20:48 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 25643,
-	"created_at": "Thu Jan 03 03:20:48 +0000 2019",
+	"tweets": 25644,
+	"created_at": "Mon Jan 07 03:20:47 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Sun Oct 14 20:13:09 +0000 2018",
+		"id": 1051566599541387300,
+		"id_str": "1051566599541387264",
+		"full_text": "you haven’t experienced real heartbreak until you’ve come across a meme you’d send to a person who you don’t talk to anymore",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			124
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 563412919,
+			"id_str": "563412919",
+			"name": "𝓁𝒾𝓁 𝓈𝓉𝒶𝓇",
+			"screen_name": "astrarchi",
+			"location": "18!🥤",
+			"description": "taurus baby ♡",
+			"url": "https://t.co/jhC008FoG4",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jhC008FoG4",
+							"expanded_url": "http://Instagram.com/astrarchi",
+							"display_url": "Instagram.com/astrarchi",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 30166,
+			"friends_count": 281,
+			"listed_count": 947,
+			"created_at": "Thu Apr 26 03:37:22 +0000 2012",
+			"favourites_count": 8822,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18333,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "642D8B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081255905449201665/JhEhNP3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081255905449201665/JhEhNP3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/563412919/1546225885",
+			"profile_link_color": "FF0000",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9280,
+		"favorite_count": 37747,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "you haven’t experienced real heartbreak until you’ve come across a meme you’d send to a person who you don’t talk to anymore"
+	},
+	{
 		"created_at": "Wed Jan 02 16:59:14 +0000 2019",
 		"id": 1080508831581511700,
 		"id_str": "1080508831581511681",
