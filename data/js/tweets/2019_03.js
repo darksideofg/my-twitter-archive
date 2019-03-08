cd3de098a6a7a9b2133df8b518ea15524d82d122
darksideofg <42369239+darksideofg@users.noreply.github.com>
2019-03-08 04:20:48 +0100

    Added 1 tweets

--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Thu Mar 07 18:34:34 +0000 2019",
+		"id": 1103683006277939200,
+		"id_str": "1103725645421596674",
+		"full_text": "straight line:\n_______________________\ndash line:\n- - - - - - - - - - - - - - -\niconic line: https://t.co/LIccWetSOa",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			92
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1103683002293542900,
+					"id_str": "1103683002293542914",
+					"indices": [
+						93,
+						116
+					],
+					"media_url": "http://pbs.twimg.com/media/D1ER19BX4AIatyi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D1ER19BX4AIatyi.jpg",
+					"url": "https://t.co/LIccWetSOa",
+					"display_url": "pic.twitter.com/LIccWetSOa",
+					"expanded_url": "https://twitter.com/its_willyu/status/1103683006277939200/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 425,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 750,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1103683002293542900,
+					"id_str": "1103683002293542914",
+					"indices": [
+						93,
+						116
+					],
+					"media_url": "http://pbs.twimg.com/media/D1ER19BX4AIatyi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D1ER19BX4AIatyi.jpg",
+					"url": "https://t.co/LIccWetSOa",
+					"display_url": "pic.twitter.com/LIccWetSOa",
+					"expanded_url": "https://twitter.com/its_willyu/status/1103683006277939200/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 425,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 750,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 725424811154018300,
+			"id_str": "725424811154018306",
+			"name": "William Yu",
+			"screen_name": "its_willyu",
+			"location": "Los Angeles, CA",
+			"description": "#StarringJohnCho / #SeeAsAmStar creator. @shortyawards Winner. Sr. Strategist @Giant_Spoon. #AAIFF41 Screenplay Comp Winner. Repped by @precociouspenny.",
+			"url": "https://t.co/bN1Fk4cWMl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bN1Fk4cWMl",
+							"expanded_url": "http://itswillyu.com",
+							"display_url": "itswillyu.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 13972,
+			"friends_count": 7999,
+			"listed_count": 79,
+			"created_at": "Wed Apr 27 20:42:07 +0000 2016",
+			"favourites_count": 18563,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6793,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1093386591295262722/TfCqa1LG_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1093386591295262722/TfCqa1LG_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/725424811154018306/1535687785",
+			"profile_link_color": "000080",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1235,
+		"favorite_count": 4055,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "straight line:\n_______________________\ndash line:\n- - - - - - - - - - - - - - -\niconic line: https://t.co/LIccWetSOa"
+	},
+	{
 		"created_at": "Wed Mar 06 17:26:39 +0000 2019",
 		"id": 1103346165939167200,
 		"id_str": "1103346165939167237",
