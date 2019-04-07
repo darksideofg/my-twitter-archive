fd8fc937e9feddf2faa164d237f76213e89fb845
darksideofg <42369239+darksideofg@users.noreply.github.com>
2019-04-07 05:20:48 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 25782,
-	"created_at": "Sat Apr 06 03:20:47 +0000 2019",
+	"tweets": 25783,
+	"created_at": "Sun Apr 07 03:20:47 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Sat Apr 06 16:32:35 +0000 2019",
+		"id": 1113201648841543700,
+		"id_str": "1114566580560642048",
+		"full_text": "me trying to figure out who everyone on twitter is subtweeting https://t.co/8PpW3505Wk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			62
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1113201561876844500,
+					"id_str": "1113201561876844544",
+					"indices": [
+						63,
+						86
+					],
+					"media_url": "http://pbs.twimg.com/media/D3Li7CsUUAABMEV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3Li7CsUUAABMEV.jpg",
+					"url": "https://t.co/8PpW3505Wk",
+					"display_url": "pic.twitter.com/8PpW3505Wk",
+					"expanded_url": "https://twitter.com/BBW_BFF/status/1113201648841543680/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 399,
+							"h": 600,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 399,
+							"h": 600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 399,
+							"h": 600,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1113201561876844500,
+					"id_str": "1113201561876844544",
+					"indices": [
+						63,
+						86
+					],
+					"media_url": "http://pbs.twimg.com/media/D3Li7CsUUAABMEV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3Li7CsUUAABMEV.jpg",
+					"url": "https://t.co/8PpW3505Wk",
+					"display_url": "pic.twitter.com/8PpW3505Wk",
+					"expanded_url": "https://twitter.com/BBW_BFF/status/1113201648841543680/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 399,
+							"h": 600,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 399,
+							"h": 600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 399,
+							"h": 600,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1113200686773076000,
+		"in_reply_to_status_id_str": "1113200686773075968",
+		"in_reply_to_user_id": 67799200,
+		"in_reply_to_user_id_str": "67799200",
+		"in_reply_to_screen_name": "BBW_BFF",
+		"user": {
+			"id": 67799200,
+			"id_str": "67799200",
+			"name": "priscilla page",
+			"screen_name": "BBW_BFF",
+			"location": "los angeles",
+			"description": "stygian as fuck https://t.co/m7uHmmvWgy",
+			"url": "https://t.co/L2gmqlZXXH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/L2gmqlZXXH",
+							"expanded_url": "http://priscillapage.contently.com",
+							"display_url": "priscillapage.contently.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/m7uHmmvWgy",
+							"expanded_url": "http://motorpsych.tumblr.com",
+							"display_url": "motorpsych.tumblr.com",
+							"indices": [
+								16,
+								39
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 54822,
+			"friends_count": 891,
+			"listed_count": 759,
+			"created_at": "Sat Aug 22 03:11:05 +0000 2009",
+			"favourites_count": 99623,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 41357,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/871847210312384512/wKG1a-si_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/871847210312384512/wKG1a-si_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/67799200/1459204633",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 83,
+		"favorite_count": 446,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "me trying to figure out who everyone on twitter is subtweeting https://t.co/8PpW3505Wk"
+	},
+	{
 		"created_at": "Fri Apr 05 06:14:58 +0000 2019",
 		"id": 1114048765374103600,
 		"id_str": "1114048765374103552",
