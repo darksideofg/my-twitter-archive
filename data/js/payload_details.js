f3c8b4310e948e92a74be91bd684409ea35b9490
darksideofg <42369239+darksideofg@users.noreply.github.com>
2019-04-11 05:20:48 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 25787,
-	"created_at": "Wed Apr 10 03:20:48 +0000 2019",
+	"tweets": 25788,
+	"created_at": "Thu Apr 11 03:20:47 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,109 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Wed Apr 10 16:29:21 +0000 2019",
+		"id": 1115952308590870500,
+		"id_str": "1116015320383328257",
+		"full_text": "MADONNA CHE SCEMA ELEONORA SEMBRI ME. #SKAMITALIA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			49
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SKAMITALIA",
+					"indices": [
+						38,
+						49
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 754893,
+			"id_str": "754893",
+			"name": "Signor Ponza",
+			"screen_name": "signorponza",
+			"location": "Milan, Lombardy",
+			"description": "Andrea is like Emma Bunton: shy on the outside, but such a nasty bitch inside | Instagram: @signorponza | @signorponzablog e #PZSQ (per 1/4) sono colpa mia",
+			"url": "https://t.co/cuX3NK80dV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/cuX3NK80dV",
+							"expanded_url": "https://www.instagram.com/signorponza/",
+							"display_url": "instagram.com/signorponza/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5679,
+			"friends_count": 1131,
+			"listed_count": 86,
+			"created_at": "Mon Feb 05 17:22:19 +0000 2007",
+			"favourites_count": 61139,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 99165,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/845678133659516929/p8HsTJKJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/845678133659516929/p8HsTJKJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/754893/1475579254",
+			"profile_link_color": "4A913C",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "it",
+		"text": "MADONNA CHE SCEMA ELEONORA SEMBRI ME. #SKAMITALIA"
+	},
+	{
 		"created_at": "Tue Apr 09 15:00:28 +0000 2019",
 		"id": 1115630563887657000,
 		"id_str": "1115630563887656961",
