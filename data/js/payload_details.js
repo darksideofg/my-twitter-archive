246880e103a57d5f7e700ff68d375210c35ff3b4
darksideofg <42369239+darksideofg@users.noreply.github.com>
2019-05-09 05:20:49 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 25804,
-	"created_at": "Wed May 08 03:20:47 +0000 2019",
+	"tweets": 25805,
+	"created_at": "Thu May 09 03:20:47 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,216 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Wed May 08 04:42:56 +0000 2019",
+		"id": 1125635535542804500,
+		"id_str": "1125984404633993216",
+		"full_text": "https://t.co/pj3WsemuiM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1125635530702508000,
+					"id_str": "1125635530702508032",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/D58PjSIU0AA4T7T.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D58PjSIU0AA4T7T.jpg",
+					"url": "https://t.co/pj3WsemuiM",
+					"display_url": "pic.twitter.com/pj3WsemuiM",
+					"expanded_url": "https://twitter.com/hdevalence/status/1125635535542804480/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 378,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1125,
+							"h": 625,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1125,
+							"h": 625,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1125635530702508000,
+					"id_str": "1125635530702508032",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/D58PjSIU0AA4T7T.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D58PjSIU0AA4T7T.jpg",
+					"url": "https://t.co/pj3WsemuiM",
+					"display_url": "pic.twitter.com/pj3WsemuiM",
+					"expanded_url": "https://twitter.com/hdevalence/status/1125635535542804480/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 378,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1125,
+							"h": 625,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1125,
+							"h": 625,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1125635530689957900,
+					"id_str": "1125635530689957888",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/D58PjSFVUAA5KZt.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D58PjSFVUAA5KZt.jpg",
+					"url": "https://t.co/pj3WsemuiM",
+					"display_url": "pic.twitter.com/pj3WsemuiM",
+					"expanded_url": "https://twitter.com/hdevalence/status/1125635535542804480/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 580,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 613,
+							"h": 719,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 613,
+							"h": 719,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 529148230,
+			"id_str": "529148230",
+			"name": "\\u221e",
+			"screen_name": "hdevalence",
+			"location": "the machinic phylum",
+			"description": "cryptography research @ZcashFoundation; interested in zero-knowledge, privacy, freedom, mathematics, & the number 24 (he/him)",
+			"url": "https://t.co/XnDQKAwOsy",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/XnDQKAwOsy",
+							"expanded_url": "http://www.hdevalence.ca",
+							"display_url": "hdevalence.ca",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3534,
+			"friends_count": 588,
+			"listed_count": 107,
+			"created_at": "Mon Mar 19 06:12:13 +0000 2012",
+			"favourites_count": 13955,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11919,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/690675173557702657/iVh_MJij_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/690675173557702657/iVh_MJij_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/529148230/1405021573",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12520,
+		"favorite_count": 59643,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/pj3WsemuiM"
+	},
+	{
 		"created_at": "Tue May 07 16:27:30 +0000 2019",
 		"id": 1125799328306667500,
 		"id_str": "1125799328306667522",
