0551bf99a0d9ba94fca24fc317c685f66f11ad1e
darksideofg <42369239+darksideofg@users.noreply.github.com>
2019-06-26 05:20:48 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 25893,
-	"created_at": "Mon Jun 24 03:20:47 +0000 2019",
+	"tweets": 25894,
+	"created_at": "Wed Jun 26 03:20:47 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,10 +1,112 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Tue Jun 25 06:28:12 +0000 2019",
+		"id": 1143405512790302700,
+		"id_str": "1143405512790302720",
+		"full_text": "Io totally Annabella e la fine del mondo #BigLittleLies",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			55
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BigLittleLies",
+					"indices": [
+						41,
+						55
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 535277787,
+			"id_str": "535277787",
+			"name": "G (̄・・̄)",
+			"screen_name": "DarkSideOfG",
+			"location": "εᴍ - Siberia",
+			"description": "“The drought was the very worst / When the flowers that we'd grown together died of thirst”",
+			"url": "https://t.co/kzM9iE3UtX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kzM9iE3UtX",
+							"expanded_url": "http://darksideofg.tk",
+							"display_url": "darksideofg.tk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 116,
+			"friends_count": 75,
+			"listed_count": 10,
+			"created_at": "Sat Mar 24 09:46:29 +0000 2012",
+			"favourites_count": 8100,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 25722,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "515151",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_link_color": "686868",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "595959",
+			"profile_text_color": "606060",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "it",
+		"text": "Io totally Annabella e la fine del mondo #BigLittleLies"
+	},
+	{
 		"created_at": "Sun Jun 23 09:06:41 +0000 2019",
 		"id": 1142720620511268900,
 		"id_str": "1142720620511268865",
-		"full_text": "I had missed this video. It’s both heartbreaking and heartwarming at the same time. Welcome to the 🏳️‍🌈 community Dan! https://t.co/Ddc6iLn5gX",
+		"full_text": "I had missed this video. It’s both heartbreaking and heartwarming at the same time. Welcome to the 🏳️🌈 community Dan! https://t.co/Ddc6iLn5gX",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -215,7 +317,7 @@ Grailbird.data.tweets_2019_06 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "I had missed this video. It’s both heartbreaking and heartwarming at the same time. Welcome to the 🏳️‍🌈 community Dan! https://t.co/Ddc6iLn5gX"
+		"text": "I had missed this video. It’s both heartbreaking and heartwarming at the same time. Welcome to the 🏳️🌈 community Dan! https://t.co/Ddc6iLn5gX"
 	},
 	{
 		"created_at": "Thu Jun 20 19:36:01 +0000 2019",
