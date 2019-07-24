e5baa1e803a772c10501ac1c37e79413e1eada98
darksideofg <42369239+darksideofg@users.noreply.github.com>
2019-07-24 05:20:48 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 25912,
-	"created_at": "Sat Jul 20 03:20:47 +0000 2019",
+	"tweets": 25913,
+	"created_at": "Wed Jul 24 03:20:47 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,111 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Tue Jul 23 13:31:03 +0000 2019",
+		"id": 1153658787548369000,
+		"id_str": "1153658787548368900",
+		"full_text": "@frangments Pensavo fosse sempre lo stesso.",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			43
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "frangments",
+					"name": "Francesco",
+					"id": 248201954,
+					"id_str": "248201954",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1153638050397921300,
+		"in_reply_to_status_id_str": "1153638050397921282",
+		"in_reply_to_user_id": 248201954,
+		"in_reply_to_user_id_str": "248201954",
+		"in_reply_to_screen_name": "frangments",
+		"user": {
+			"id": 535277787,
+			"id_str": "535277787",
+			"name": "G (̄・・̄)",
+			"screen_name": "DarkSideOfG",
+			"location": "εᴍ - Siberia",
+			"description": "“The drought was the very worst / When the flowers that we'd grown together died of thirst”",
+			"url": "https://t.co/kzM9iE3UtX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kzM9iE3UtX",
+							"expanded_url": "http://darksideofg.tk",
+							"display_url": "darksideofg.tk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 117,
+			"friends_count": 75,
+			"listed_count": 10,
+			"created_at": "Sat Mar 24 09:46:29 +0000 2012",
+			"favourites_count": 8128,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 25746,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "515151",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_link_color": "686868",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "595959",
+			"profile_text_color": "606060",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "it",
+		"text": "@frangments Pensavo fosse sempre lo stesso."
+	},
+	{
 		"created_at": "Sat Jul 20 00:34:42 +0000 2019",
 		"id": 1152027018982068200,
 		"id_str": "1152376249609838592",
@@ -28,7 +133,7 @@ Grailbird.data.tweets_2019_07 =
 			"name": "Emily Kager",
 			"screen_name": "EmilyKager",
 			"location": "SF Bay Area",
-			"description": "Trying to make tech better. #Android + @Kotlin @Mozilla 🦊. Dog Lover. Runner. Vegan. Open Source. Jumpsuits. Feminist. She/Her 👩🏼‍🎓 @Penn @MillsCollege",
+			"description": "Trying to make tech better. #Android + @Kotlin @Mozilla 🦊. Dog Lover. Runner. Vegan. Open Source. Jumpsuits. Feminist. She/Her 👩🏼🎓 @Penn @MillsCollege",
 			"url": "https://t.co/jjw3I5vWwr",
 			"entities": {
 				"url": {
@@ -120,7 +225,7 @@ Grailbird.data.tweets_2019_07 =
 		"user": {
 			"id": 2417571,
 			"id_str": "2417571",
-			"name": "Cesar Torres ♥️🇺🇸🏳️‍🌈",
+			"name": "Cesar Torres ♥️🇺🇸🏳️🌈",
 			"screen_name": "cesart",
 			"location": "Manhattan",
 			"description": "Partner at https://t.co/otEDUIOxRS — Past: Chief Creative at GM, Design Director at Sidecar; Asana, Milk Inc, frog design, Apple; Austin & SF",
