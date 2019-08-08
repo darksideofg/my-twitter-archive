8f4a6e024ab49bef42f409efb4d9ae367f25ac2c
darksideofg <42369239+darksideofg@users.noreply.github.com>
2019-08-08 05:20:49 +0200

    Added 1 tweets

--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Wed Aug 07 20:09:48 +0000 2019",
+		"id": 1159194952242475000,
+		"id_str": "1159194952242475009",
+		"full_text": "@Bert_oldo No, meglio non rivelare la mia Marissa interiore https://t.co/6mPcHXV8Tw",
+		"truncated": false,
+		"display_text_range": [
+			11,
+			59
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Bert_oldo",
+					"name": "Bert_TH_oldo",
+					"id": 414705729,
+					"id_str": "414705729",
+					"indices": [
+						0,
+						10
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1159194920416157700,
+					"id_str": "1159194920416157698",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EBZJo3DXoAI5NT3.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EBZJo3DXoAI5NT3.jpg",
+					"url": "https://t.co/6mPcHXV8Tw",
+					"display_url": "pic.twitter.com/6mPcHXV8Tw",
+					"expanded_url": "https://twitter.com/DarkSideOfG/status/1159194952242475009/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 244,
+							"h": 150,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 244,
+							"h": 150,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 244,
+							"h": 150,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1159194920416157700,
+					"id_str": "1159194920416157698",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EBZJo3DXoAI5NT3.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EBZJo3DXoAI5NT3.jpg",
+					"url": "https://t.co/6mPcHXV8Tw",
+					"display_url": "pic.twitter.com/6mPcHXV8Tw",
+					"expanded_url": "https://twitter.com/DarkSideOfG/status/1159194952242475009/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 244,
+							"h": 150,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 244,
+							"h": 150,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 244,
+							"h": 150,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							122,
+							75
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EBZJo3DXoAI5NT3.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1159192951668519000,
+		"in_reply_to_status_id_str": "1159192951668518912",
+		"in_reply_to_user_id": 414705729,
+		"in_reply_to_user_id_str": "414705729",
+		"in_reply_to_screen_name": "Bert_oldo",
+		"user": {
+			"id": 535277787,
+			"id_str": "535277787",
+			"name": "G (̄・・̄)",
+			"screen_name": "DarkSideOfG",
+			"location": "εᴍ - Siberia",
+			"description": "“The drought was the very worst / When the flowers that we'd grown together died of thirst”",
+			"url": "https://t.co/kzM9iE3UtX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kzM9iE3UtX",
+							"expanded_url": "http://darksideofg.tk",
+							"display_url": "darksideofg.tk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 117,
+			"friends_count": 75,
+			"listed_count": 10,
+			"created_at": "Sat Mar 24 09:46:29 +0000 2012",
+			"favourites_count": 8146,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 25756,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "515151",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_link_color": "686868",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "595959",
+			"profile_text_color": "606060",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "it",
+		"text": "@Bert_oldo No, meglio non rivelare la mia Marissa interiore https://t.co/6mPcHXV8Tw"
+	},
+	{
 		"created_at": "Tue Aug 06 18:04:42 +0000 2019",
 		"id": 1158801085252931600,
 		"id_str": "1158801085252931588",
