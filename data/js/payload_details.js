bed9f77f4b53025621c178780e355e27c6213980
darksideofg <42369239+darksideofg@users.noreply.github.com>
2019-08-15 05:20:48 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 25928,
-	"created_at": "Wed Aug 14 03:20:48 +0000 2019",
+	"tweets": 25929,
+	"created_at": "Thu Aug 15 03:20:47 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Wed Aug 14 17:02:04 +0000 2019",
+		"id": 1161467965210017800,
+		"id_str": "1161684425861718017",
+		"full_text": "Sorry, everyone's sitting in the gravity well. https://t.co/UcUo0tT04P",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			46
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1161467961271603200,
+					"id_str": "1161467961271603201",
+					"indices": [
+						47,
+						70
+					],
+					"media_url": "http://pbs.twimg.com/media/EB5c9QGXUAExIzQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EB5c9QGXUAExIzQ.jpg",
+					"url": "https://t.co/UcUo0tT04P",
+					"display_url": "pic.twitter.com/UcUo0tT04P",
+					"expanded_url": "https://twitter.com/ibogost/status/1161467965210017793/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 928,
+							"h": 523,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 928,
+							"h": 523,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1161467961271603200,
+					"id_str": "1161467961271603201",
+					"indices": [
+						47,
+						70
+					],
+					"media_url": "http://pbs.twimg.com/media/EB5c9QGXUAExIzQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EB5c9QGXUAExIzQ.jpg",
+					"url": "https://t.co/UcUo0tT04P",
+					"display_url": "pic.twitter.com/UcUo0tT04P",
+					"expanded_url": "https://twitter.com/ibogost/status/1161467965210017793/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 928,
+							"h": 523,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 928,
+							"h": 523,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 6825792,
+			"id_str": "6825792",
+			"name": "Ian Bogost",
+			"screen_name": "ibogost",
+			"location": "Atlanta, GA",
+			"description": "Writer, game designer, and sorbetto influencer. Professor @GeorgiaTech, Contributing Editor @TheAtlantic. Also: Persuasive Games, @ObjectsObjects.",
+			"url": "https://t.co/rpSiyMSN93",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/rpSiyMSN93",
+							"expanded_url": "http://bogost.com",
+							"display_url": "bogost.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 101736,
+			"friends_count": 4939,
+			"listed_count": 2366,
+			"created_at": "Fri Jun 15 04:29:43 +0000 2007",
+			"favourites_count": 46474,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3055,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "5B3C19",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1150025902501355520/7BQ95ELx_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1150025902501355520/7BQ95ELx_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/6825792/1562778280",
+			"profile_link_color": "D0E6E3",
+			"profile_sidebar_border_color": "5B3C19",
+			"profile_sidebar_fill_color": "D8A636",
+			"profile_text_color": "231F20",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 169,
+		"favorite_count": 716,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Sorry, everyone's sitting in the gravity well. https://t.co/UcUo0tT04P"
+	},
+	{
 		"created_at": "Tue Aug 13 18:08:52 +0000 2019",
 		"id": 1139771904447803400,
 		"id_str": "1161338849072746498",
@@ -25,7 +199,7 @@ Grailbird.data.tweets_2019_08 =
 		"user": {
 			"id": 606441611,
 			"id_str": "606441611",
-			"name": "Michael 🏳️‍🌈",
+			"name": "Michael 🏳️🌈",
 			"screen_name": "Home_Halfway",
 			"location": "Chicago, IL",
 			"description": "Producer & booking manager of @SpitfireComedy at @iOChicago. Co-admin of @FeministNetflix with @Kelsey_Caine.",
