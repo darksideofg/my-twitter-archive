23c28e63152dbbc6791dd02832eba0f65d3105c2
darksideofg <42369239+darksideofg@users.noreply.github.com>
2019-09-11 05:20:49 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 25935,
-	"created_at": "Tue Sep 10 03:20:47 +0000 2019",
+	"tweets": 25936,
+	"created_at": "Wed Sep 11 03:20:48 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,193 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Tue Sep 10 18:04:21 +0000 2019",
+		"id": 1171484237947527200,
+		"id_str": "1171484569406558208",
+		"full_text": "“Slofies” https://t.co/3aKd9KXmfb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			9
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1171484208335769600,
+					"id_str": "1171484208335769632",
+					"indices": [
+						10,
+						33
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EEHyrkHVUCA-ztL.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EEHyrkHVUCA-ztL.jpg",
+					"url": "https://t.co/3aKd9KXmfb",
+					"display_url": "pic.twitter.com/3aKd9KXmfb",
+					"expanded_url": "https://twitter.com/tiffanyarment/status/1171484237947527170/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 576,
+							"h": 304,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 576,
+							"h": 304,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 576,
+							"h": 304,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1171484208335769600,
+					"id_str": "1171484208335769632",
+					"indices": [
+						10,
+						33
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EEHyrkHVUCA-ztL.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EEHyrkHVUCA-ztL.jpg",
+					"url": "https://t.co/3aKd9KXmfb",
+					"display_url": "pic.twitter.com/3aKd9KXmfb",
+					"expanded_url": "https://twitter.com/tiffanyarment/status/1171484237947527170/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 576,
+							"h": 304,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 576,
+							"h": 304,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 576,
+							"h": 304,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							36,
+							19
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EEHyrkHVUCA-ztL.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 25178436,
+			"id_str": "25178436",
+			"name": "Tiff",
+			"screen_name": "tiffanyarment",
+			"location": "New York",
+			"description": "“You’re an artist the way you see the world.”",
+			"url": "https://t.co/1PuEVeAbIZ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1PuEVeAbIZ",
+							"expanded_url": "http://www.tiffanyarment.com",
+							"display_url": "tiffanyarment.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 9572,
+			"friends_count": 225,
+			"listed_count": 332,
+			"created_at": "Wed Mar 18 23:06:13 +0000 2009",
+			"favourites_count": 10724,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 10947,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "352726",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/707613172249858048/CvoTgB8r_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/707613172249858048/CvoTgB8r_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/25178436/1511272971",
+			"profile_link_color": "D02B55",
+			"profile_sidebar_border_color": "829D5E",
+			"profile_sidebar_fill_color": "99CC33",
+			"profile_text_color": "3E4415",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 11,
+		"favorite_count": 49,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "“Slofies” https://t.co/3aKd9KXmfb"
+	},
+	{
 		"created_at": "Mon Sep 09 09:47:58 +0000 2019",
 		"id": 1170997265600827400,
 		"id_str": "1170997265600827393",
