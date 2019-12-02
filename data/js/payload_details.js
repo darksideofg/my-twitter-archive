802d3ed4b1cbd25e7a5e352294d32c5c7f2d8a2d
darksideofg <42369239+darksideofg@users.noreply.github.com>
2019-12-02 04:21:02 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 25969,
-	"created_at": "Sat Nov 30 03:20:47 +0000 2019",
+	"tweets": 25970,
+	"created_at": "Mon Dec 02 03:21:01 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_12.js
@@ -0,0 +1,530 @@
+Grailbird.data.tweets_2019_12 = 
+[
+	{
+		"created_at": "Sun Dec 01 08:33:34 +0000 2019",
+		"id": 1201056735567523800,
+		"id_str": "1201056735567523841",
+		"full_text": "Iconic https://t.co/Om688dvdQJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			6
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Om688dvdQJ",
+					"expanded_url": "https://twitter.com/Campaignmag/status/1200833125745451015",
+					"display_url": "twitter.com/Campaignmag/st…",
+					"indices": [
+						7,
+						30
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 535277787,
+			"id_str": "535277787",
+			"name": "G (̄・・̄)",
+			"screen_name": "DarkSideOfG",
+			"location": "εᴍ - Siberia",
+			"description": "“The drought was the very worst / When the flowers that we'd grown together died of thirst”",
+			"url": "https://t.co/kzM9iE3UtX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kzM9iE3UtX",
+							"expanded_url": "http://darksideofg.tk",
+							"display_url": "darksideofg.tk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 114,
+			"friends_count": 75,
+			"listed_count": 9,
+			"created_at": "Sat Mar 24 09:46:29 +0000 2012",
+			"favourites_count": 8280,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 25799,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "515151",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_link_color": "686868",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "595959",
+			"profile_text_color": "606060",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1200833125745451000,
+		"quoted_status_id_str": "1200833125745451015",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Om688dvdQJ",
+			"expanded": "https://twitter.com/Campaignmag/status/1200833125745451015",
+			"display": "twitter.com/Campaignmag/st…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Nov 30 17:45:02 +0000 2019",
+			"id": 1200833125745451000,
+			"id_str": "1200833125745451015",
+			"full_text": "Lidl has enlisted pop artist KT Tunstall to perform at a series of gigs across Scotland as the supermarket opens its 100th store there https://t.co/RZbfvykRWS https://t.co/siCGBsUBgw",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				158
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/RZbfvykRWS",
+						"expanded_url": "https://buff.ly/33vb7G6",
+						"display_url": "buff.ly/33vb7G6",
+						"indices": [
+							135,
+							158
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1200833122838962200,
+						"id_str": "1200833122838962177",
+						"indices": [
+							159,
+							182
+						],
+						"media_url": "http://pbs.twimg.com/media/EKo3XOlW4AEZHsF.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EKo3XOlW4AEZHsF.jpg",
+						"url": "https://t.co/siCGBsUBgw",
+						"display_url": "pic.twitter.com/siCGBsUBgw",
+						"expanded_url": "https://twitter.com/Campaignmag/status/1200833125745451015/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 454,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1222,
+								"h": 815,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 800,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"orig": {
+								"faces": [
+									{
+										"x": 258,
+										"y": 473,
+										"h": 47,
+										"w": 47
+									},
+									{
+										"x": 798,
+										"y": 75,
+										"h": 76,
+										"w": 76
+									},
+									{
+										"x": 189,
+										"y": 503,
+										"h": 78,
+										"w": 78
+									},
+									{
+										"x": 279,
+										"y": 35,
+										"h": 264,
+										"w": 264
+									}
+								]
+							},
+							"small": {
+								"faces": [
+									{
+										"x": 143,
+										"y": 263,
+										"h": 26,
+										"w": 26
+									},
+									{
+										"x": 444,
+										"y": 41,
+										"h": 42,
+										"w": 42
+									},
+									{
+										"x": 105,
+										"y": 279,
+										"h": 43,
+										"w": 43
+									},
+									{
+										"x": 155,
+										"y": 19,
+										"h": 146,
+										"w": 146
+									}
+								]
+							},
+							"large": {
+								"faces": [
+									{
+										"x": 258,
+										"y": 473,
+										"h": 47,
+										"w": 47
+									},
+									{
+										"x": 798,
+										"y": 75,
+										"h": 76,
+										"w": 76
+									},
+									{
+										"x": 189,
+										"y": 503,
+										"h": 78,
+										"w": 78
+									},
+									{
+										"x": 279,
+										"y": 35,
+										"h": 264,
+										"w": 264
+									}
+								]
+							},
+							"medium": {
+								"faces": [
+									{
+										"x": 253,
+										"y": 464,
+										"h": 46,
+										"w": 46
+									},
+									{
+										"x": 783,
+										"y": 73,
+										"h": 74,
+										"w": 74
+									},
+									{
+										"x": 185,
+										"y": 493,
+										"h": 76,
+										"w": 76
+									},
+									{
+										"x": 273,
+										"y": 34,
+										"h": 259,
+										"w": 259
+									}
+								]
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1200833122838962200,
+						"id_str": "1200833122838962177",
+						"indices": [
+							159,
+							182
+						],
+						"media_url": "http://pbs.twimg.com/media/EKo3XOlW4AEZHsF.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EKo3XOlW4AEZHsF.jpg",
+						"url": "https://t.co/siCGBsUBgw",
+						"display_url": "pic.twitter.com/siCGBsUBgw",
+						"expanded_url": "https://twitter.com/Campaignmag/status/1200833125745451015/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 454,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1222,
+								"h": 815,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 800,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"orig": {
+								"faces": [
+									{
+										"x": 258,
+										"y": 473,
+										"h": 47,
+										"w": 47
+									},
+									{
+										"x": 798,
+										"y": 75,
+										"h": 76,
+										"w": 76
+									},
+									{
+										"x": 189,
+										"y": 503,
+										"h": 78,
+										"w": 78
+									},
+									{
+										"x": 279,
+										"y": 35,
+										"h": 264,
+										"w": 264
+									}
+								]
+							},
+							"small": {
+								"faces": [
+									{
+										"x": 143,
+										"y": 263,
+										"h": 26,
+										"w": 26
+									},
+									{
+										"x": 444,
+										"y": 41,
+										"h": 42,
+										"w": 42
+									},
+									{
+										"x": 105,
+										"y": 279,
+										"h": 43,
+										"w": 43
+									},
+									{
+										"x": 155,
+										"y": 19,
+										"h": 146,
+										"w": 146
+									}
+								]
+							},
+							"large": {
+								"faces": [
+									{
+										"x": 258,
+										"y": 473,
+										"h": 47,
+										"w": 47
+									},
+									{
+										"x": 798,
+										"y": 75,
+										"h": 76,
+										"w": 76
+									},
+									{
+										"x": 189,
+										"y": 503,
+										"h": 78,
+										"w": 78
+									},
+									{
+										"x": 279,
+										"y": 35,
+										"h": 264,
+										"w": 264
+									}
+								]
+							},
+							"medium": {
+								"faces": [
+									{
+										"x": 253,
+										"y": 464,
+										"h": 46,
+										"w": 46
+									},
+									{
+										"x": 783,
+										"y": 73,
+										"h": 74,
+										"w": 74
+									},
+									{
+										"x": 185,
+										"y": 493,
+										"h": 76,
+										"w": 76
+									},
+									{
+										"x": 273,
+										"y": 34,
+										"h": 259,
+										"w": 259
+									}
+								]
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 16468227,
+				"id_str": "16468227",
+				"name": "Campaign",
+				"screen_name": "Campaignmag",
+				"location": "London",
+				"description": "The world’s leading magazine for the advertising, marketing & media communities.\n@CampaignBrands @CampaignMedia @CampaignExpUK @CampaignTechUK\n@CampaignJobsUK",
+				"url": "https://t.co/RgSfLi4EFN",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/RgSfLi4EFN",
+								"expanded_url": "http://www.campaignlive.co.uk",
+								"display_url": "campaignlive.co.uk",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 194954,
+				"friends_count": 3850,
+				"listed_count": 4526,
+				"created_at": "Fri Sep 26 14:25:15 +0000 2008",
+				"favourites_count": 3386,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 71712,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "E1EEEF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1048172361965613057/76wGYcAu_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1048172361965613057/76wGYcAu_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/16468227/1572857390",
+				"profile_link_color": "090B0B",
+				"profile_sidebar_border_color": "E1EEEF",
+				"profile_sidebar_fill_color": "F91F1F",
+				"profile_text_color": "0E0C0C",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 40,
+			"favorite_count": 234,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Iconic https://t.co/Om688dvdQJ"
+	}
+]
