1066392bdf92d1743627327d9cb1e5f738868be8
darksideofg <42369239+darksideofg@users.noreply.github.com>
2020-01-10 04:20:48 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 25991,
-	"created_at": "Mon Jan 06 03:20:47 +0000 2020",
+	"tweets": 25992,
+	"created_at": "Fri Jan 10 03:20:47 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,103 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Thu Jan 09 09:37:36 +0000 2020",
+		"id": 1214999342509113300,
+		"id_str": "1215205973687775232",
+		"full_text": "I [38F] don't feel welcome in my husband [35M]'s home country. His family [93F, 98M, 71M, 72F, 37M, 37F] and subjects [all ages] are very demanding. How can I convince him to set boundaries?",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			190
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 92095451,
+			"id_str": "92095451",
+			"name": "Meredith Haggerty",
+			"screen_name": "manymanywords",
+			"location": "Brooklyn, NY",
+			"description": "I hate capitalism but I love amenities. Deputy editor, @thegoods by @voxdotcom. Formerly @ racked. She/her.",
+			"url": "https://t.co/UNbOAWjR3f",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UNbOAWjR3f",
+							"expanded_url": "http://dailym.ai/1py78Sx",
+							"display_url": "dailym.ai/1py78Sx",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 12576,
+			"friends_count": 4443,
+			"listed_count": 246,
+			"created_at": "Mon Nov 23 19:37:03 +0000 2009",
+			"favourites_count": 50245,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 3776,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1138666398807404544/TQbkvXTL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1138666398807404544/TQbkvXTL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/92095451/1360468396",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5046,
+		"favorite_count": 45894,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "I [38F] don't feel welcome in my husband [35M]'s home country. His family [93F, 98M, 71M, 72F, 37M, 37F] and subjects [all ages] are very demanding. How can I convince him to set boundaries?"
+	},
+	{
 		"created_at": "Sun Jan 05 11:10:26 +0000 2020",
 		"id": 1213779785614319600,
 		"id_str": "1213779785614319616",
