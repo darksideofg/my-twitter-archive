af43311077e461481bdb2e680e219a1b7cd81693
darksideofg <42369239+darksideofg@users.noreply.github.com>
2020-02-03 04:20:48 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26007,
-	"created_at": "Fri Jan 31 03:20:47 +0000 2020",
+	"tweets": 26008,
+	"created_at": "Mon Feb 03 03:20:47 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_02.js
@@ -0,0 +1,213 @@
+Grailbird.data.tweets_2020_02 = 
+[
+	{
+		"created_at": "Sun Feb 02 22:10:02 +0000 2020",
+		"id": 1223325228694212600,
+		"id_str": "1224092639953289217",
+		"full_text": "Do you ever https://t.co/uDqY3uFwzs",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			11
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1223325207282307000,
+					"id_str": "1223325207282307074",
+					"indices": [
+						12,
+						35
+					],
+					"media_url": "http://pbs.twimg.com/media/EPofy2jX0AIRnT3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EPofy2jX0AIRnT3.jpg",
+					"url": "https://t.co/uDqY3uFwzs",
+					"display_url": "pic.twitter.com/uDqY3uFwzs",
+					"expanded_url": "https://twitter.com/HotPaperComics/status/1223325228694212609/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1223325207282307000,
+					"id_str": "1223325207282307074",
+					"indices": [
+						12,
+						35
+					],
+					"media_url": "http://pbs.twimg.com/media/EPofy2jX0AIRnT3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EPofy2jX0AIRnT3.jpg",
+					"url": "https://t.co/uDqY3uFwzs",
+					"display_url": "pic.twitter.com/uDqY3uFwzs",
+					"expanded_url": "https://twitter.com/HotPaperComics/status/1223325228694212609/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873422783119806500,
+			"id_str": "873422783119806464",
+			"name": "Martin Rosner",
+			"screen_name": "HotPaperComics",
+			"location": "",
+			"description": "I do Hot Paper Comics. Nothing smart to add to this description.\nInstagram: https://t.co/hfmlB8Qt3E\nhttps://t.co/89AIXoRMBl",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/hfmlB8Qt3E",
+							"expanded_url": "http://instagram.com/martin_rosner/",
+							"display_url": "instagram.com/martin_rosner/",
+							"indices": [
+								76,
+								99
+							]
+						},
+						{
+							"url": "https://t.co/89AIXoRMBl",
+							"expanded_url": "https://www.patreon.com/HotPaperComics/posts",
+							"display_url": "patreon.com/HotPaperComics…",
+							"indices": [
+								100,
+								123
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 24634,
+			"friends_count": 272,
+			"listed_count": 140,
+			"created_at": "Sat Jun 10 06:13:13 +0000 2017",
+			"favourites_count": 28147,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 5964,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1128638074215383040/by-XEAdY_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1128638074215383040/by-XEAdY_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873422783119806464/1529689098",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 58027,
+		"favorite_count": 151191,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Do you ever https://t.co/uDqY3uFwzs"
+	}
+]
