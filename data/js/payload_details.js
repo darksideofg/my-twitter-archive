8ee0dba4c9b5dc45cb56dfd3c43d2ef740f556a7
darksideofg <42369239+darksideofg@users.noreply.github.com>
2020-02-06 04:20:48 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26009,
-	"created_at": "Wed Feb 05 03:20:48 +0000 2020",
+	"tweets": 26011,
+	"created_at": "Thu Feb 06 03:20:47 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,297 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Wed Feb 05 17:28:44 +0000 2020",
+		"id": 1225093491996397600,
+		"id_str": "1225109012221517825",
+		"full_text": "If you don’t want Spotify to take over podcasts for themselves, the best things you can do are:\n\n- Don’t listen there. Most other podcast apps are better anyway!\n\n- Stop listening to any shows that become Spotify exclusives, and tell the creators why they lost you as a listener.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1225092674442625000,
+		"in_reply_to_status_id_str": "1225092674442625029",
+		"in_reply_to_user_id": 14231571,
+		"in_reply_to_user_id_str": "14231571",
+		"in_reply_to_screen_name": "marcoarment",
+		"user": {
+			"id": 14231571,
+			"id_str": "14231571",
+			"name": "Marco Arment",
+			"screen_name": "marcoarment",
+			"location": "New York",
+			"description": "Husband of renowned podcaster @tiffanyarment.",
+			"url": "https://t.co/uJY2D2tEgI",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/uJY2D2tEgI",
+							"expanded_url": "http://www.marco.org/",
+							"display_url": "marco.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 116504,
+			"friends_count": 165,
+			"listed_count": 7006,
+			"created_at": "Thu Mar 27 01:01:42 +0000 2008",
+			"favourites_count": 20649,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2265,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1005850629028352000/jHsNM3wM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1005850629028352000/jHsNM3wM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14231571/1505914114",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 234,
+		"favorite_count": 430,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "If you don’t want Spotify to take over podcasts for themselves, the best things you can do are:\n\n- Don’t listen there. Most other podcast apps are better anyway!\n\n- Stop listening to any shows that become Spotify exclusives, and tell the creators why they lost you as a listener."
+	},
+	{
+		"created_at": "Wed Feb 05 07:23:17 +0000 2020",
+		"id": 1224956646344085500,
+		"id_str": "1224956646344085504",
+		"full_text": "KFC sneaks ads onto Spotify Premium by taking over artist profiles https://t.co/xSYa2aR5eX https://t.co/CQZzk9AEJl",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			90
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/xSYa2aR5eX",
+					"expanded_url": "https://www.theverge.com/2020/2/4/21122257/kfc-arabia-spotify-premium-memac-ogilvy",
+					"display_url": "theverge.com/2020/2/4/21122…",
+					"indices": [
+						67,
+						90
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1224956615000055800,
+					"id_str": "1224956615000055808",
+					"indices": [
+						91,
+						114
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EP_rjR5X4AAUmg_.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EP_rjR5X4AAUmg_.jpg",
+					"url": "https://t.co/CQZzk9AEJl",
+					"display_url": "pic.twitter.com/CQZzk9AEJl",
+					"expanded_url": "https://twitter.com/DarkSideOfG/status/1224956646344085504/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 440,
+							"h": 300,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 440,
+							"h": 300,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 440,
+							"h": 300,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1224956615000055800,
+					"id_str": "1224956615000055808",
+					"indices": [
+						91,
+						114
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EP_rjR5X4AAUmg_.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EP_rjR5X4AAUmg_.jpg",
+					"url": "https://t.co/CQZzk9AEJl",
+					"display_url": "pic.twitter.com/CQZzk9AEJl",
+					"expanded_url": "https://twitter.com/DarkSideOfG/status/1224956646344085504/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 440,
+							"h": 300,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 440,
+							"h": 300,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 440,
+							"h": 300,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							22,
+							15
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EP_rjR5X4AAUmg_.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 535277787,
+			"id_str": "535277787",
+			"name": "G (̄・・̄)",
+			"screen_name": "DarkSideOfG",
+			"location": "εᴍ - Siberia",
+			"description": "“The drought was the very worst / When the flowers that we'd grown together died of thirst”",
+			"url": "https://t.co/kzM9iE3UtX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kzM9iE3UtX",
+							"expanded_url": "http://darksideofg.tk",
+							"display_url": "darksideofg.tk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 118,
+			"friends_count": 74,
+			"listed_count": 8,
+			"created_at": "Sat Mar 24 09:46:29 +0000 2012",
+			"favourites_count": 8339,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 25845,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "515151",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_link_color": "686868",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "595959",
+			"profile_text_color": "606060",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "KFC sneaks ads onto Spotify Premium by taking over artist profiles https://t.co/xSYa2aR5eX https://t.co/CQZzk9AEJl"
+	},
+	{
 		"created_at": "Tue Feb 04 20:22:10 +0000 2020",
 		"id": 1224790271961043000,
 		"id_str": "1224790271961042944",
