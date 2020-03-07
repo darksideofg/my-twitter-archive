03afd9acdffc7e09623d88ec1ba1aba273cfff40
darksideofg <42369239+darksideofg@users.noreply.github.com>
2020-03-07 04:20:49 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26028,
-	"created_at": "Mon Mar 02 03:20:47 +0000 2020",
+	"tweets": 26029,
+	"created_at": "Sat Mar 07 03:20:48 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Fri Mar 06 20:14:58 +0000 2020",
+		"id": 1236018570397724700,
+		"id_str": "1236022480482992130",
+		"full_text": "Interessante riflessione sulle diseguaglianze e i privilegi che stanno emergendo in questa situazione di emergenza https://t.co/iyzYrsGT35",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			138
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/iyzYrsGT35",
+					"expanded_url": "https://www.nytimes.com/2020/03/05/opinion/coronavirus-quarantine-hermit-tech.html?referringSource=articleShare",
+					"display_url": "nytimes.com/2020/03/05/opi…",
+					"indices": [
+						115,
+						138
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 754893,
+			"id_str": "754893",
+			"name": "Signor Ponza",
+			"screen_name": "signorponza",
+			"location": "Milan, Lombardy",
+			"description": "In onda con #Popper ogni domenica dalle 19:30 alle 21:00 su Radio Stonata \n📷 @signorponza",
+			"url": "https://t.co/ENU6YSN4Ra",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ENU6YSN4Ra",
+							"expanded_url": "https://tinyletter.com/signorponza",
+							"display_url": "tinyletter.com/signorponza",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5780,
+			"friends_count": 1128,
+			"listed_count": 88,
+			"created_at": "Mon Feb 05 17:22:19 +0000 2007",
+			"favourites_count": 65666,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 102207,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1224763227722207233/-zcOnKBy_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1224763227722207233/-zcOnKBy_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/754893/1475579254",
+			"profile_link_color": "4A913C",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 8,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "it",
+		"text": "Interessante riflessione sulle diseguaglianze e i privilegi che stanno emergendo in questa situazione di emergenza https://t.co/iyzYrsGT35"
+	},
+	{
 		"created_at": "Sun Mar 01 18:41:55 +0000 2020",
 		"id": 1234187125093957600,
 		"id_str": "1234187125093957633",
