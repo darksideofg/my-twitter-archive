f44c7f052c7d7b0a72b682e4ac30a9b48f0c6045
darksideofg <42369239+darksideofg@users.noreply.github.com>
2020-04-26 05:20:48 +0200

    Added 1 tweets

--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Sat Apr 25 17:33:52 +0000 2020",
+		"id": 1254099926537195500,
+		"id_str": "1254101332329467908",
+		"full_text": "Apparently our tune “Doom Days” is a year old today. Happy birthday to this depressing little bastard. \n\n(I still stand by every word of it) \n\nhttps://t.co/fYmhUb1pcN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			166
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/fYmhUb1pcN",
+					"expanded_url": "https://youtu.be/IOX30CHr4JY",
+					"display_url": "youtu.be/IOX30CHr4JY",
+					"indices": [
+						143,
+						166
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21433995,
+			"id_str": "21433995",
+			"name": "BASTILLE",
+			"screen_name": "bastilledan",
+			"location": "London",
+			"description": "Dan from Bastille; probably talking about films & other people's music // works at @BLPRecords",
+			"url": "https://t.co/QxKOO8C3dr",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/QxKOO8C3dr",
+							"expanded_url": "http://www.bastillebastille.com/",
+							"display_url": "bastillebastille.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1440742,
+			"friends_count": 108,
+			"listed_count": 2568,
+			"created_at": "Fri Feb 20 21:07:37 +0000 2009",
+			"favourites_count": 1644,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 16945,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E7E3DD",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1123955766866710528/uSrab_FR_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1123955766866710528/uSrab_FR_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21433995/1576079706",
+			"profile_link_color": "AD2F1A",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EECEBF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 891,
+		"favorite_count": 3915,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Apparently our tune “Doom Days” is a year old today. Happy birthday to this depressing little bastard. \n\n(I still stand by every word of it) \n\nhttps://t.co/fYmhUb1pcN"
+	},
+	{
 		"created_at": "Fri Apr 24 12:47:07 +0000 2020",
 		"id": 1253666783552209000,
 		"id_str": "1253666783552208897",
