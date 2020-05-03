001eb489c8795c994dedf2a1cbb8e90eba947491
darksideofg <42369239+darksideofg@users.noreply.github.com>
2020-05-03 05:20:48 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26060,
-	"created_at": "Sat May 02 03:20:47 +0000 2020",
+	"tweets": 26061,
+	"created_at": "Sun May 03 03:20:47 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sat May 02 06:29:48 +0000 2020",
+		"id": 1256294567562920000,
+		"id_str": "1256470928617115650",
+		"full_text": "My [48M] girlfriend [32F] is mad at me because I melted down on Twitter, promised to sell almost all of my physical possessions (including our house), and may have violated SEC regulations. AITA? https://t.co/3QJPA65Pwb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			195
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1256294563146236000,
+					"id_str": "1256294563146235904",
+					"indices": [
+						196,
+						219
+					],
+					"media_url": "http://pbs.twimg.com/media/EW9BP8GWAAAHCQU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW9BP8GWAAAHCQU.jpg",
+					"url": "https://t.co/3QJPA65Pwb",
+					"display_url": "pic.twitter.com/3QJPA65Pwb",
+					"expanded_url": "https://twitter.com/_Jon_Green/status/1256294567562919936/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 750,
+							"h": 891,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 572,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 750,
+							"h": 891,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1256294563146236000,
+					"id_str": "1256294563146235904",
+					"indices": [
+						196,
+						219
+					],
+					"media_url": "http://pbs.twimg.com/media/EW9BP8GWAAAHCQU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW9BP8GWAAAHCQU.jpg",
+					"url": "https://t.co/3QJPA65Pwb",
+					"display_url": "pic.twitter.com/3QJPA65Pwb",
+					"expanded_url": "https://twitter.com/_Jon_Green/status/1256294567562919936/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 750,
+							"h": 891,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 572,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 750,
+							"h": 891,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1256294563146354700,
+					"id_str": "1256294563146354692",
+					"indices": [
+						196,
+						219
+					],
+					"media_url": "http://pbs.twimg.com/media/EW9BP8GX0AQ18rW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW9BP8GX0AQ18rW.jpg",
+					"url": "https://t.co/3QJPA65Pwb",
+					"display_url": "pic.twitter.com/3QJPA65Pwb",
+					"expanded_url": "https://twitter.com/_Jon_Green/status/1256294567562919936/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 750,
+							"h": 581,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 527,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 750,
+							"h": 581,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 493387905,
+			"id_str": "493387905",
+			"name": "Jon Green",
+			"screen_name": "_Jon_Green",
+			"location": "Columbus, OH",
+			"description": "Ph.D. candidate @osupolisci. Co-founder @DataProgress. I don't know when the next poll is coming out.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5692,
+			"friends_count": 1299,
+			"listed_count": 139,
+			"created_at": "Wed Feb 15 19:04:29 +0000 2012",
+			"favourites_count": 41373,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 11814,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1070547188126769152/IbBJRi_6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1070547188126769152/IbBJRi_6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/493387905/1533262503",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3217,
+		"favorite_count": 18845,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "My [48M] girlfriend [32F] is mad at me because I melted down on Twitter, promised to sell almost all of my physical possessions (including our house), and may have violated SEC regulations. AITA? https://t.co/3QJPA65Pwb"
+	},
+	{
 		"created_at": "Fri May 01 16:20:24 +0000 2020",
 		"id": 1256257170775191600,
 		"id_str": "1256257170775191552",
