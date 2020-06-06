638c3b97d71238458aaba392dfa6a873abc0c483
darksideofg <42369239+darksideofg@users.noreply.github.com>
2020-06-06 05:20:48 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26078,
-	"created_at": "Wed Jun 03 03:20:48 +0000 2020",
+	"tweets": 26079,
+	"created_at": "Sat Jun 06 03:20:47 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,216 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Fri Jun 05 20:16:08 +0000 2020",
+		"id": 1268919762962264000,
+		"id_str": "1269000070046846976",
+		"full_text": "dril candle tweet vs LA budget https://t.co/cWnG1ijKfR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			30
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1268919678224732200,
+					"id_str": "1268919678224732160",
+					"indices": [
+						31,
+						54
+					],
+					"media_url": "http://pbs.twimg.com/media/EZwbuW0WkAAtpDi.png",
+					"media_url_https": "https://pbs.twimg.com/media/EZwbuW0WkAAtpDi.png",
+					"url": "https://t.co/cWnG1ijKfR",
+					"display_url": "pic.twitter.com/cWnG1ijKfR",
+					"expanded_url": "https://twitter.com/Glenthefossa/status/1268919762962264064/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 800,
+							"h": 533,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 533,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1268919678224732200,
+					"id_str": "1268919678224732160",
+					"indices": [
+						31,
+						54
+					],
+					"media_url": "http://pbs.twimg.com/media/EZwbuW0WkAAtpDi.png",
+					"media_url_https": "https://pbs.twimg.com/media/EZwbuW0WkAAtpDi.png",
+					"url": "https://t.co/cWnG1ijKfR",
+					"display_url": "pic.twitter.com/cWnG1ijKfR",
+					"expanded_url": "https://twitter.com/Glenthefossa/status/1268919762962264064/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 800,
+							"h": 533,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 533,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				},
+				{
+					"id": 1268919714614513700,
+					"id_str": "1268919714614513666",
+					"indices": [
+						31,
+						54
+					],
+					"media_url": "http://pbs.twimg.com/media/EZwbweYWkAIaJCf.png",
+					"media_url_https": "https://pbs.twimg.com/media/EZwbweYWkAIaJCf.png",
+					"url": "https://t.co/cWnG1ijKfR",
+					"display_url": "pic.twitter.com/cWnG1ijKfR",
+					"expanded_url": "https://twitter.com/Glenthefossa/status/1268919762962264064/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 800,
+							"h": 685,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 582,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 685,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 49288590,
+			"id_str": "49288590",
+			"name": "Glen ♉ bIm",
+			"screen_name": "Glenthefossa",
+			"location": "he/him, 33, married",
+			"description": "Illustrator. 🔞 Nazis and Pepes can fuck right off.",
+			"url": "https://t.co/aey3ksXS8y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aey3ksXS8y",
+							"expanded_url": "http://furaffinity.net/user/glenthefossa",
+							"display_url": "furaffinity.net/user/glenthefo…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4888,
+			"friends_count": 4553,
+			"listed_count": 23,
+			"created_at": "Sun Jun 21 10:46:16 +0000 2009",
+			"favourites_count": 121988,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 74689,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1234663543238062087/ZWNwRHVk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1234663543238062087/ZWNwRHVk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/49288590/1560713500",
+			"profile_link_color": "B3956F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "B8B6B2",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12585,
+		"favorite_count": 40600,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": true,
+		"lang": "en",
+		"text": "dril candle tweet vs LA budget https://t.co/cWnG1ijKfR"
+	},
+	{
 		"created_at": "Tue Jun 02 07:58:10 +0000 2020",
 		"id": 1267727192898834400,
 		"id_str": "1267727192898834432",
