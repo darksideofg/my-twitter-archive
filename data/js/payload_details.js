e7d0eea015dfee5dc45fcdfe71ea0e7817a5e430
darksideofg <42369239+darksideofg@users.noreply.github.com>
2020-07-02 05:20:49 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26088,
-	"created_at": "Wed Jun 24 03:20:48 +0000 2020",
+	"tweets": 26089,
+	"created_at": "Thu Jul 02 03:20:48 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_07.js
@@ -0,0 +1,164 @@
+Grailbird.data.tweets_2020_07 = 
+[
+	{
+		"created_at": "Wed Jul 01 15:21:16 +0000 2020",
+		"id": 1278091924948357000,
+		"id_str": "1278347949559382016",
+		"full_text": "Unmuting myself just to say “ok” in the Zoom meeting. https://t.co/rYIJihBW4P",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			53
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1278091920032649200,
+					"id_str": "1278091920032649217",
+					"indices": [
+						54,
+						77
+					],
+					"media_url": "http://pbs.twimg.com/media/Ebyx1GPX0AE52o_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ebyx1GPX0AE52o_.jpg",
+					"url": "https://t.co/rYIJihBW4P",
+					"display_url": "pic.twitter.com/rYIJihBW4P",
+					"expanded_url": "https://twitter.com/teblue/status/1278091924948357120/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 750,
+							"h": 520,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 471,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 750,
+							"h": 520,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1278091920032649200,
+					"id_str": "1278091920032649217",
+					"indices": [
+						54,
+						77
+					],
+					"media_url": "http://pbs.twimg.com/media/Ebyx1GPX0AE52o_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ebyx1GPX0AE52o_.jpg",
+					"url": "https://t.co/rYIJihBW4P",
+					"display_url": "pic.twitter.com/rYIJihBW4P",
+					"expanded_url": "https://twitter.com/teblue/status/1278091924948357120/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 750,
+							"h": 520,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 471,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 750,
+							"h": 520,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 150846070,
+			"id_str": "150846070",
+			"name": "Theophilus",
+			"screen_name": "teblue",
+			"location": "",
+			"description": "I retweet a lot.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 636,
+			"friends_count": 1334,
+			"listed_count": 10,
+			"created_at": "Wed Jun 02 00:04:35 +0000 2010",
+			"favourites_count": 170067,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 92428,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1245188150907068421/M1Weyobr_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1245188150907068421/M1Weyobr_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/150846070/1582140543",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 46353,
+		"favorite_count": 292729,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Unmuting myself just to say “ok” in the Zoom meeting. https://t.co/rYIJihBW4P"
+	}
+]
