b4fa0dd1c659aa48a352d93fe0aad7a0b59b391d
darksideofg <42369239+darksideofg@users.noreply.github.com>
2020-09-15 05:20:48 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26110,
-	"created_at": "Mon Sep 14 03:20:47 +0000 2020",
+	"tweets": 26111,
+	"created_at": "Tue Sep 15 03:20:47 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Mon Sep 14 20:57:30 +0000 2020",
+		"id": 1305141773711310800,
+		"id_str": "1305611656911872000",
+		"full_text": "Mentally, I'm here. https://t.co/qqfMVLuPOy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			19
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1305141379149033500,
+					"id_str": "1305141379149033473",
+					"indices": [
+						20,
+						43
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1305141379149033473/pu/img/0yF_sSklnmEPKGIv.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1305141379149033473/pu/img/0yF_sSklnmEPKGIv.jpg",
+					"url": "https://t.co/qqfMVLuPOy",
+					"display_url": "pic.twitter.com/qqfMVLuPOy",
+					"expanded_url": "https://twitter.com/IsaacFitzgerald/status/1305141773711310852/video/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 640,
+							"h": 788,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 640,
+							"h": 788,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 552,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1305141379149033500,
+					"id_str": "1305141379149033473",
+					"indices": [
+						20,
+						43
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1305141379149033473/pu/img/0yF_sSklnmEPKGIv.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1305141379149033473/pu/img/0yF_sSklnmEPKGIv.jpg",
+					"url": "https://t.co/qqfMVLuPOy",
+					"display_url": "pic.twitter.com/qqfMVLuPOy",
+					"expanded_url": "https://twitter.com/IsaacFitzgerald/status/1305141773711310852/video/1",
+					"type": "video",
+					"sizes": {
+						"large": {
+							"w": 640,
+							"h": 788,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 640,
+							"h": 788,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 552,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							160,
+							197
+						],
+						"duration_millis": 4700,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1305141379149033473/pu/pl/AeYiJ7bdJUaDOCMW.m3u8?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1305141379149033473/pu/vid/640x788/QnLDVX-DNFfx2UzK.mp4?tag=10"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1305141379149033473/pu/vid/320x394/L_-A-DfgeGa0CSyk.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1305141379149033473/pu/vid/360x442/ErAMik5OOmmT7tCd.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21926404,
+			"id_str": "21926404",
+			"name": "Isaac Fitzgerald🤞🏻🖤",
+			"screen_name": "IsaacFitzgerald",
+			"location": "Brooklyn",
+			"description": "Pen & Ink | Knives & Ink | How to Be a Pirate | Dirtbag, Massachusetts | YA forthcoming | Repped by @SheedyLit + @TuesdayAgency | Life mistakes are my co-pilot.",
+			"url": "https://t.co/uB2eK1TO1b",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/uB2eK1TO1b",
+							"expanded_url": "http://www.isaacfitzgerald.net",
+							"display_url": "isaacfitzgerald.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 34398,
+			"friends_count": 993,
+			"listed_count": 676,
+			"created_at": "Wed Feb 25 22:04:45 +0000 2009",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 104595,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1255719836799426567/PmQJbQHc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1255719836799426567/PmQJbQHc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21926404/1583163755",
+			"profile_link_color": "038543",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "F6F6F6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 22170,
+		"favorite_count": 84585,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Mentally, I'm here. https://t.co/qqfMVLuPOy"
+	},
+	{
 		"created_at": "Sun Sep 13 09:48:03 +0000 2020",
 		"id": 1305080797309829000,
 		"id_str": "1305080797309829120",
