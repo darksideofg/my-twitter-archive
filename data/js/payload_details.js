3330080503d8bcdddff18baaeb6107b28ef16b46
darksideofg <42369239+darksideofg@users.noreply.github.com>
2020-10-08 05:20:48 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26118,
-	"created_at": "Sun Oct 04 03:20:47 +0000 2020",
+	"tweets": 26119,
+	"created_at": "Thu Oct 08 03:20:47 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,280 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Wed Oct 07 06:06:58 +0000 2020",
+		"id": 1313266129880711200,
+		"id_str": "1313722467014316039",
+		"full_text": "cinema can't top this https://t.co/JuVZzvIAkk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			45
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1195325162041811000,
+					"id_str": "1195325162041810944",
+					"indices": [
+						22,
+						45
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1195325162041810944/pu/img/r7tfu01gRX2yXS1v.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1195325162041810944/pu/img/r7tfu01gRX2yXS1v.jpg",
+					"url": "https://t.co/JuVZzvIAkk",
+					"display_url": "pic.twitter.com/JuVZzvIAkk",
+					"expanded_url": "https://twitter.com/lixxmcshammer/status/1195325281793429505/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1195325281793429500,
+					"source_status_id_str": "1195325281793429505",
+					"source_user_id": 1003197468568379400,
+					"source_user_id_str": "1003197468568379395"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1195325162041811000,
+					"id_str": "1195325162041810944",
+					"indices": [
+						22,
+						45
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1195325162041810944/pu/img/r7tfu01gRX2yXS1v.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1195325162041810944/pu/img/r7tfu01gRX2yXS1v.jpg",
+					"url": "https://t.co/JuVZzvIAkk",
+					"display_url": "pic.twitter.com/JuVZzvIAkk",
+					"expanded_url": "https://twitter.com/lixxmcshammer/status/1195325281793429505/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1195325281793429500,
+					"source_status_id_str": "1195325281793429505",
+					"source_user_id": 1003197468568379400,
+					"source_user_id_str": "1003197468568379395",
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 123762,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1195325162041810944/pu/vid/1280x720/sr-qntqp9DLKnfdM.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1195325162041810944/pu/pl/QwUfRbojN2XZC4qX.m3u8?tag=10"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1195325162041810944/pu/vid/480x270/dXL2QJuj7UlXqZR6.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1195325162041810944/pu/vid/640x360/rPVgiRX8r6bqRk91.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false,
+						"source_user": {
+							"id": 1003197468568379400,
+							"id_str": "1003197468568379395",
+							"name": "𝐋",
+							"screen_name": "Iickmileyshamma",
+							"location": "Paris, France",
+							"description": "erotica",
+							"url": "https://t.co/GIAf0dk7Z7",
+							"entities": {
+								"url": {
+									"urls": [
+										{
+											"url": "https://t.co/GIAf0dk7Z7",
+											"expanded_url": "http://mileyl.ink/midnightsky",
+											"display_url": "mileyl.ink/midnightsky",
+											"indices": [
+												0,
+												23
+											]
+										}
+									]
+								},
+								"description": {
+									"urls": []
+								}
+							},
+							"protected": false,
+							"followers_count": 3888,
+							"friends_count": 3121,
+							"listed_count": 12,
+							"created_at": "Sun Jun 03 08:51:29 +0000 2018",
+							"favourites_count": 102309,
+							"utc_offset": null,
+							"time_zone": null,
+							"geo_enabled": true,
+							"verified": false,
+							"statuses_count": 64414,
+							"lang": null,
+							"contributors_enabled": false,
+							"is_translator": false,
+							"is_translation_enabled": false,
+							"profile_background_color": "F5F8FA",
+							"profile_background_image_url": null,
+							"profile_background_image_url_https": null,
+							"profile_background_tile": false,
+							"profile_image_url": "http://pbs.twimg.com/profile_images/1310261501702418434/T5c9d74N_normal.jpg",
+							"profile_image_url_https": "https://pbs.twimg.com/profile_images/1310261501702418434/T5c9d74N_normal.jpg",
+							"profile_banner_url": "https://pbs.twimg.com/profile_banners/1003197468568379395/1597413494",
+							"profile_link_color": "1DA1F2",
+							"profile_sidebar_border_color": "C0DEED",
+							"profile_sidebar_fill_color": "DDEEF6",
+							"profile_text_color": "333333",
+							"profile_use_background_image": true,
+							"has_extended_profile": false,
+							"default_profile": true,
+							"default_profile_image": false,
+							"following": false,
+							"follow_request_sent": false,
+							"notifications": false,
+							"translator_type": "none"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1241141106118713300,
+			"id_str": "1241141106118713350",
+			"name": "out of context hannah montana",
+			"screen_name": "OCHannahMontana",
+			"location": "Malibu, CA",
+			"description": "oooooaahhwowowahhhohyeeah #BlackLivesSTILLMatter",
+			"url": "https://t.co/GIAf0dk7Z7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GIAf0dk7Z7",
+							"expanded_url": "http://mileyl.ink/midnightsky",
+							"display_url": "mileyl.ink/midnightsky",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 47334,
+			"friends_count": 4,
+			"listed_count": 41,
+			"created_at": "Fri Mar 20 23:15:02 +0000 2020",
+			"favourites_count": 9353,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4897,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1311588195260411906/239PuAGL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1311588195260411906/239PuAGL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1241141106118713350/1592696272",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 13636,
+		"favorite_count": 80302,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "cinema can't top this https://t.co/JuVZzvIAkk"
+	},
+	{
 		"created_at": "Sat Oct 03 20:53:32 +0000 2020",
 		"id": 1312496026318598100,
 		"id_str": "1312496026318598145",
