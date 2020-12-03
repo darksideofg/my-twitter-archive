1d21f49f409974f626af96ff1f3dcc03ae230cc2
darksideofg <42369239+darksideofg@users.noreply.github.com>
2020-12-03 04:20:49 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26128,
-	"created_at": "Sat Nov 28 03:20:48 +0000 2020",
+	"tweets": 26129,
+	"created_at": "Thu Dec 03 03:20:48 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_12.js
@@ -0,0 +1,363 @@
+Grailbird.data.tweets_2020_12 = 
+[
+	{
+		"created_at": "Wed Dec 02 15:19:28 +0000 2020",
+		"id": 1334144808693223400,
+		"id_str": "1334155228808146946",
+		"full_text": "Okay so while my new re-records are NOT done, my friend @VancityReynolds asked me if he could use a snippet of one for a LOLsome commercial he wrote so...here’s a sneak peak of Love Story! Working hard to get the music to you soon!! https://t.co/0vBFXxaRXR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			232
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "VancityReynolds",
+					"name": "Ryan Reynolds",
+					"id": 2893511188,
+					"id_str": "2893511188",
+					"indices": [
+						56,
+						72
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/0vBFXxaRXR",
+					"expanded_url": "https://twitter.com/vancityreynolds/status/1334134478042587137",
+					"display_url": "twitter.com/vancityreynold…",
+					"indices": [
+						233,
+						256
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 17919972,
+			"id_str": "17919972",
+			"name": "Taylor Swift",
+			"screen_name": "taylorswift13",
+			"location": "",
+			"description": "you drew stars around my scars",
+			"url": "https://t.co/1UodIYtA0r",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1UodIYtA0r",
+							"expanded_url": "https://taylor.lnk.to/longpond",
+							"display_url": "taylor.lnk.to/longpond",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 87594484,
+			"friends_count": 0,
+			"listed_count": 112807,
+			"created_at": "Sat Dec 06 10:10:54 +0000 2008",
+			"favourites_count": 1028,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 552,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1286270219980242945/70DWScEH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1286270219980242945/70DWScEH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/17919972/1606292371",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1334134478042587100,
+		"quoted_status_id_str": "1334134478042587137",
+		"quoted_status_permalink": {
+			"url": "https://t.co/0vBFXxaRXR",
+			"expanded": "https://twitter.com/vancityreynolds/status/1334134478042587137",
+			"display": "twitter.com/vancityreynold…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Dec 02 13:57:01 +0000 2020",
+			"id": 1334134478042587100,
+			"id_str": "1334134478042587137",
+			"full_text": "A match made in hell is still a @match. Latest work from @MaximumEffort #2020LoveStory https://t.co/kGL02PIqin",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				86
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "2020LoveStory",
+						"indices": [
+							72,
+							86
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "Match",
+						"name": "Match",
+						"id": 19013415,
+						"id_str": "19013415",
+						"indices": [
+							32,
+							38
+						]
+					},
+					{
+						"screen_name": "MaximumEffort",
+						"name": "Maximum Effort",
+						"id": 984905195816546300,
+						"id_str": "984905195816546304",
+						"indices": [
+							57,
+							71
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1333906027390017500,
+						"id_str": "1333906027390017536",
+						"indices": [
+							87,
+							110
+						],
+						"media_url": "http://pbs.twimg.com/amplify_video_thumb/1333906027390017536/img/n0LB6GjgGGsHIJCk.jpg",
+						"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1333906027390017536/img/n0LB6GjgGGsHIJCk.jpg",
+						"url": "https://t.co/kGL02PIqin",
+						"display_url": "pic.twitter.com/kGL02PIqin",
+						"expanded_url": "https://twitter.com/VancityReynolds/status/1334134478042587137/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1333906027390017500,
+						"id_str": "1333906027390017536",
+						"indices": [
+							87,
+							110
+						],
+						"media_url": "http://pbs.twimg.com/amplify_video_thumb/1333906027390017536/img/n0LB6GjgGGsHIJCk.jpg",
+						"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1333906027390017536/img/n0LB6GjgGGsHIJCk.jpg",
+						"url": "https://t.co/kGL02PIqin",
+						"display_url": "pic.twitter.com/kGL02PIqin",
+						"expanded_url": "https://twitter.com/VancityReynolds/status/1334134478042587137/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 87880,
+							"variants": [
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1333906027390017536/vid/640x360/40hZXVkd42Aw4kd9.mp4?tag=13"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1333906027390017536/vid/1280x720/pXyW4W2irOGCbDLl.mp4?tag=13"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/amplify_video/1333906027390017536/pl/nF8UZGUkEFhqKrui.m3u8?tag=13"
+								},
+								{
+									"bitrate": 288000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1333906027390017536/vid/480x270/jJD_r1mBZuAaksyS.mp4?tag=13"
+								}
+							]
+						},
+						"additional_media_info": {
+							"title": "",
+							"description": "",
+							"embeddable": true,
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2893511188,
+				"id_str": "2893511188",
+				"name": "Ryan Reynolds",
+				"screen_name": "VancityReynolds",
+				"location": "🇨🇦",
+				"description": "owner: @aviationgin - @MintMobile - @maximumeffort",
+				"url": "https://t.co/VsQdkj5wNf",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/VsQdkj5wNf",
+								"expanded_url": "http://www.aviationgin.com/delivered",
+								"display_url": "aviationgin.com/delivered",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 16891139,
+				"friends_count": 952,
+				"listed_count": 10332,
+				"created_at": "Wed Nov 26 16:12:27 +0000 2014",
+				"favourites_count": 22299,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 2108,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1299844050208555008/7wMQaJRA_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1299844050208555008/7wMQaJRA_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2893511188/1568928229",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 25192,
+			"favorite_count": 170916,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 44504,
+		"favorite_count": 273362,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Okay so while my new re-records are NOT done, my friend @VancityReynolds asked me if he could use a snippet of one for a LOLsome commercial he wrote so...here’s a sneak peak of Love Story! Working hard to get the music to you soon!! https://t.co/0vBFXxaRXR"
+	}
+]
