ac1c97f00acd1ed4dec4928c72a466048f5aaa43
darksideofg <42369239+darksideofg@users.noreply.github.com>
2021-03-02 04:20:49 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26170,
-	"created_at": "Mon Mar 01 03:20:47 +0000 2021",
+	"tweets": 26171,
+	"created_at": "Tue Mar 02 03:20:49 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_03.js
@@ -0,0 +1,98 @@
+Grailbird.data.tweets_2021_03 = 
+[
+	{
+		"created_at": "Mon Mar 01 21:15:11 +0000 2021",
+		"id": 1366493450984546300,
+		"id_str": "1366497265687404561",
+		"full_text": "March                my ex-boyfriend\n\n                 🤝                                 \n             \n  \"it'll be different this time\"",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			136
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 22785963,
+			"id_str": "22785963",
+			"name": "Alexis Gay",
+			"screen_name": "yayalexisgay",
+			"location": "San Francisco, CA",
+			"description": "Comedyperson | Host of @NonTechnicalPod | prev. Biz Ops & Partnerships @patreon | Type A NYC transplant pretending to be chill enough to live in the bay 🙃",
+			"url": "https://t.co/d3uN2kMmc3",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/d3uN2kMmc3",
+							"expanded_url": "http://youtube.com/alexisgay",
+							"display_url": "youtube.com/alexisgay",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 62235,
+			"friends_count": 724,
+			"listed_count": 299,
+			"created_at": "Wed Mar 04 15:22:09 +0000 2009",
+			"favourites_count": 28604,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6492,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1334189139080654849/1SDCGHVe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1334189139080654849/1SDCGHVe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/22785963/1607531069",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "030303",
+			"profile_sidebar_fill_color": "AEB1F5",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 39,
+		"favorite_count": 951,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "March                my ex-boyfriend\n\n                 🤝                                 \n             \n  \"it'll be different this time\""
+	}
+]
