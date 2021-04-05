0fe00d7cc0f0494c0d0cc77a4acc697713eaa4e3
darksideofg <42369239+darksideofg@users.noreply.github.com>
2021-04-05 05:20:48 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26189,
-	"created_at": "Tue Mar 30 03:20:47 +0000 2021",
+	"tweets": 26190,
+	"created_at": "Mon Apr 05 03:20:47 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_04.js
@@ -0,0 +1,360 @@
+Grailbird.data.tweets_2021_04 = 
+[
+	{
+		"created_at": "Sun Apr 04 17:02:06 +0000 2021",
+		"id": 1378754764788351000,
+		"id_str": "1378754764788350976",
+		"full_text": "Peak tv https://t.co/Ks9PIwZZD3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			7
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Ks9PIwZZD3",
+					"expanded_url": "https://twitter.com/PRADAESQUE/status/1378128561815572484",
+					"display_url": "twitter.com/PRADAESQUE/sta…",
+					"indices": [
+						8,
+						31
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 535277787,
+			"id_str": "535277787",
+			"name": "G (̄・・̄)",
+			"screen_name": "DarkSideOfG",
+			"location": "εᴍ - Siberia",
+			"description": "“The drought was the very worst / When the flowers that we'd grown together died of thirst”",
+			"url": "https://t.co/kzM9iE3UtX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kzM9iE3UtX",
+							"expanded_url": "http://darksideofg.tk",
+							"display_url": "darksideofg.tk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 106,
+			"friends_count": 73,
+			"listed_count": 6,
+			"created_at": "Sat Mar 24 09:46:29 +0000 2012",
+			"favourites_count": 8388,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 25975,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "515151",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_link_color": "686868",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "595959",
+			"profile_text_color": "606060",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1378128561815572500,
+		"quoted_status_id_str": "1378128561815572484",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Ks9PIwZZD3",
+			"expanded": "https://twitter.com/PRADAESQUE/status/1378128561815572484",
+			"display": "twitter.com/PRADAESQUE/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Apr 02 23:33:48 +0000 2021",
+			"id": 1378128561815572500,
+			"id_str": "1378128561815572484",
+			"full_text": "2004 was one for the books https://t.co/MUOMToV8PN",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				26
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1378128557734498300,
+						"id_str": "1378128557734498307",
+						"indices": [
+							27,
+							50
+						],
+						"media_url": "http://pbs.twimg.com/media/EyAYnyKWEAMugwW.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EyAYnyKWEAMugwW.jpg",
+						"url": "https://t.co/MUOMToV8PN",
+						"display_url": "pic.twitter.com/MUOMToV8PN",
+						"expanded_url": "https://twitter.com/PRADAESQUE/status/1378128561815572484/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 676,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 827,
+								"h": 822,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 827,
+								"h": 822,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1378128557734498300,
+						"id_str": "1378128557734498307",
+						"indices": [
+							27,
+							50
+						],
+						"media_url": "http://pbs.twimg.com/media/EyAYnyKWEAMugwW.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EyAYnyKWEAMugwW.jpg",
+						"url": "https://t.co/MUOMToV8PN",
+						"display_url": "pic.twitter.com/MUOMToV8PN",
+						"expanded_url": "https://twitter.com/PRADAESQUE/status/1378128561815572484/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 676,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 827,
+								"h": 822,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 827,
+								"h": 822,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1378128557742952400,
+						"id_str": "1378128557742952448",
+						"indices": [
+							27,
+							50
+						],
+						"media_url": "http://pbs.twimg.com/media/EyAYnyMXEAA_h_E.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EyAYnyMXEAA_h_E.jpg",
+						"url": "https://t.co/MUOMToV8PN",
+						"display_url": "pic.twitter.com/MUOMToV8PN",
+						"expanded_url": "https://twitter.com/PRADAESQUE/status/1378128561815572484/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 675,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 827,
+								"h": 821,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 827,
+								"h": 821,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1378128557734555600,
+						"id_str": "1378128557734555657",
+						"indices": [
+							27,
+							50
+						],
+						"media_url": "http://pbs.twimg.com/media/EyAYnyKW8AkfWST.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EyAYnyKW8AkfWST.jpg",
+						"url": "https://t.co/MUOMToV8PN",
+						"display_url": "pic.twitter.com/MUOMToV8PN",
+						"expanded_url": "https://twitter.com/PRADAESQUE/status/1378128561815572484/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 827,
+								"h": 825,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 827,
+								"h": 825,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 678,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1286549141963841500,
+				"id_str": "1286549141963841536",
+				"name": "mirren☆",
+				"screen_name": "PRADAESQUE",
+				"location": "22 • she/her • infj",
+				"description": "*･ﾟ:✧ just a girl who loves film & fashion ✧:･ﾟ*",
+				"url": "https://t.co/RHGrZ3qWOf",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/RHGrZ3qWOf",
+								"expanded_url": "https://curiouscat.qa/PRADAESQUE",
+								"display_url": "curiouscat.qa/PRADAESQUE",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3262,
+				"friends_count": 930,
+				"listed_count": 73,
+				"created_at": "Fri Jul 24 06:30:02 +0000 2020",
+				"favourites_count": 43372,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 12791,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1378467538582048771/WRSpjX44_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1378467538582048771/WRSpjX44_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1286549141963841536/1617486767",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 71353,
+			"favorite_count": 427486,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Peak tv https://t.co/Ks9PIwZZD3"
+	}
+]
