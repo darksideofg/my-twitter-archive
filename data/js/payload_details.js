4803ee51e2ee43bfff36f6f8d9b5f2a70dfdeff4
darksideofg <42369239+darksideofg@users.noreply.github.com>
2021-05-02 05:20:48 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26209,
-	"created_at": "Thu Apr 29 03:20:47 +0000 2021",
+	"tweets": 26210,
+	"created_at": "Sun May 02 03:20:47 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_05.js
@@ -0,0 +1,302 @@
+Grailbird.data.tweets_2021_05 = 
+[
+	{
+		"created_at": "Sat May 01 06:24:18 +0000 2021",
+		"id": 1388378728170983400,
+		"id_str": "1388378728170983425",
+		"full_text": "https://t.co/47ccfPVXpA https://t.co/9rcVUnyY74",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			23
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/47ccfPVXpA",
+					"expanded_url": "https://twitter.com/film_girl/status/1388228497022218248",
+					"display_url": "twitter.com/film_girl/stat…",
+					"indices": [
+						0,
+						23
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1388378695639961600,
+					"id_str": "1388378695639961600",
+					"indices": [
+						24,
+						47
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/E0SDEVdXEAA0WkH.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/E0SDEVdXEAA0WkH.jpg",
+					"url": "https://t.co/9rcVUnyY74",
+					"display_url": "pic.twitter.com/9rcVUnyY74",
+					"expanded_url": "https://twitter.com/DarkSideOfG/status/1388378728170983425/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 480,
+							"h": 268,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 480,
+							"h": 268,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 268,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1388378695639961600,
+					"id_str": "1388378695639961600",
+					"indices": [
+						24,
+						47
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/E0SDEVdXEAA0WkH.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/E0SDEVdXEAA0WkH.jpg",
+					"url": "https://t.co/9rcVUnyY74",
+					"display_url": "pic.twitter.com/9rcVUnyY74",
+					"expanded_url": "https://twitter.com/DarkSideOfG/status/1388378728170983425/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 480,
+							"h": 268,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 480,
+							"h": 268,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 268,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							120,
+							67
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/E0SDEVdXEAA0WkH.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 535277787,
+			"id_str": "535277787",
+			"name": "G (̄・・̄)",
+			"screen_name": "DarkSideOfG",
+			"location": "εᴍ - Siberia",
+			"description": "“The drought was the very worst / When the flowers that we'd grown together died of thirst”",
+			"url": "https://t.co/kzM9iE3UtX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kzM9iE3UtX",
+							"expanded_url": "http://darksideofg.tk",
+							"display_url": "darksideofg.tk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 105,
+			"friends_count": 74,
+			"listed_count": 6,
+			"created_at": "Sat Mar 24 09:46:29 +0000 2012",
+			"favourites_count": 7488,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 25887,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "515151",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_link_color": "686868",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "595959",
+			"profile_text_color": "606060",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1388228497022218200,
+		"quoted_status_id_str": "1388228497022218248",
+		"quoted_status_permalink": {
+			"url": "https://t.co/47ccfPVXpA",
+			"expanded": "https://twitter.com/film_girl/status/1388228497022218248",
+			"display": "twitter.com/film_girl/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Apr 30 20:27:20 +0000 2021",
+			"id": 1388228497022218200,
+			"id_str": "1388228497022218248",
+			"full_text": "Just some food for thought. Obviously size != influence, but this whole week has really made me reflect on why I respected and took David and Jason's words as near dogma, when in truth, they are just very good marketers who convinced us all they were more important than they are.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				280
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+			"in_reply_to_status_id": 1388228137498996700,
+			"in_reply_to_status_id_str": "1388228137498996736",
+			"in_reply_to_user_id": 9866582,
+			"in_reply_to_user_id_str": "9866582",
+			"in_reply_to_screen_name": "film_girl",
+			"user": {
+				"id": 9866582,
+				"id_str": "9866582",
+				"name": "Christina Warren",
+				"screen_name": "film_girl",
+				"location": "SEA via NYC",
+				"description": "Sr Cloud Advocate Microsoft was: @Gizmodo @Mashable hosts: @ovrtrd @_RocketFM Networked: 5G Future. Loves media and tech. opinions = own christina@christina.is",
+				"url": "https://t.co/pU60V1ZNSc",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/pU60V1ZNSc",
+								"expanded_url": "http://www.christina.is",
+								"display_url": "christina.is",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 95011,
+				"friends_count": 13595,
+				"listed_count": 4896,
+				"created_at": "Thu Nov 01 22:45:37 +0000 2007",
+				"favourites_count": 112924,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 135660,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1308921120360288256/17MPMqYU_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1308921120360288256/17MPMqYU_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/9866582/1398181728",
+				"profile_link_color": "E81C4F",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 80,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/47ccfPVXpA https://t.co/9rcVUnyY74"
+	}
+]
