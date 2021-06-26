ed884c9c8c9fe3e2cce090c97fa197d80361c60f
darksideofg <42369239+darksideofg@users.noreply.github.com>
2021-06-26 05:20:48 +0200

    Added 1 tweets

--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,176 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Fri Jun 25 16:21:20 +0000 2021",
+		"id": 1408196424974618600,
+		"id_str": "1408460311787720709",
+		"full_text": "#TheGoodFight dropping the opening credits 48 minutes into the episode https://t.co/crFhy0As22",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			70
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TheGoodFight",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1408196422030270500,
+					"id_str": "1408196422030270471",
+					"indices": [
+						71,
+						94
+					],
+					"media_url": "http://pbs.twimg.com/media/E4rrL1UXIAcYih3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4rrL1UXIAcYih3.jpg",
+					"url": "https://t.co/crFhy0As22",
+					"display_url": "pic.twitter.com/crFhy0As22",
+					"expanded_url": "https://twitter.com/abookisatardis/status/1408196424974618627/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 448,
+							"h": 336,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 448,
+							"h": 336,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 448,
+							"h": 336,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1408196422030270500,
+					"id_str": "1408196422030270471",
+					"indices": [
+						71,
+						94
+					],
+					"media_url": "http://pbs.twimg.com/media/E4rrL1UXIAcYih3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4rrL1UXIAcYih3.jpg",
+					"url": "https://t.co/crFhy0As22",
+					"display_url": "pic.twitter.com/crFhy0As22",
+					"expanded_url": "https://twitter.com/abookisatardis/status/1408196424974618627/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 448,
+							"h": 336,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 448,
+							"h": 336,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 448,
+							"h": 336,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3540929895,
+			"id_str": "3540929895",
+			"name": "gi",
+			"screen_name": "abookisatardis",
+			"location": "",
+			"description": "Exclamation point overuser!!! Constantly waffling on about stuff I love!!!\n\nShe/her",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 91,
+			"friends_count": 819,
+			"listed_count": 1,
+			"created_at": "Fri Sep 04 04:15:38 +0000 2015",
+			"favourites_count": 30763,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 31886,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1396491536771256338/gBpZkYOi_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1396491536771256338/gBpZkYOi_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3540929895/1621784514",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 26,
+		"favorite_count": 165,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#TheGoodFight dropping the opening credits 48 minutes into the episode https://t.co/crFhy0As22"
+	},
+	{
 		"created_at": "Tue Jun 22 21:56:14 +0000 2021",
 		"id": 1407457427138695200,
 		"id_str": "1407457427138695177",
