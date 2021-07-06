89ef308c83ef9d25a041a4055fda296bbdb08554
darksideofg <42369239+darksideofg@users.noreply.github.com>
2021-07-06 05:20:47 +0200

    Added 1 tweets

--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,401 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Mon Jul 05 16:18:56 +0000 2021",
+		"id": 1412083584941400000,
+		"id_str": "1412083584941400074",
+		"full_text": "Un po’ voglio il tagliere con scritto Alice ADSL. https://t.co/trPQJjSiG0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			49
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/trPQJjSiG0",
+					"expanded_url": "https://twitter.com/SnazzyQ/status/1412083263925981185",
+					"display_url": "twitter.com/SnazzyQ/status…",
+					"indices": [
+						50,
+						73
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 535277787,
+			"id_str": "535277787",
+			"name": "G (̄・・̄)",
+			"screen_name": "DarkSideOfG",
+			"location": "εᴍ - Siberia",
+			"description": "“The drought was the very worst / When the flowers that we'd grown together died of thirst”",
+			"url": "https://t.co/kzM9iE3UtX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kzM9iE3UtX",
+							"expanded_url": "http://darksideofg.tk",
+							"display_url": "darksideofg.tk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 106,
+			"friends_count": 75,
+			"listed_count": 6,
+			"created_at": "Sat Mar 24 09:46:29 +0000 2012",
+			"favourites_count": 7566,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 25984,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "515151",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_link_color": "686868",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "595959",
+			"profile_text_color": "606060",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1412083263925981200,
+		"quoted_status_id_str": "1412083263925981185",
+		"quoted_status_permalink": {
+			"url": "https://t.co/trPQJjSiG0",
+			"expanded": "https://twitter.com/SnazzyQ/status/1412083263925981185",
+			"display": "twitter.com/SnazzyQ/status…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Jul 05 16:17:40 +0000 2021",
+			"id": 1412083263925981200,
+			"id_str": "1412083263925981185",
+			"full_text": "so xfinity has a merch store\n\nwhy?\n\nidk, but these are some of my favorites https://t.co/ekOF9d3C8Y",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				75
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1412083235710857200,
+						"id_str": "1412083235710857221",
+						"indices": [
+							76,
+							99
+						],
+						"media_url": "http://pbs.twimg.com/media/E5i6OKnUcAUrsL-.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/E5i6OKnUcAUrsL-.jpg",
+						"url": "https://t.co/ekOF9d3C8Y",
+						"display_url": "pic.twitter.com/ekOF9d3C8Y",
+						"expanded_url": "https://twitter.com/SnazzyQ/status/1412083263925981185/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1125,
+								"h": 1525,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 885,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 502,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1412083235710857200,
+						"id_str": "1412083235710857221",
+						"indices": [
+							76,
+							99
+						],
+						"media_url": "http://pbs.twimg.com/media/E5i6OKnUcAUrsL-.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/E5i6OKnUcAUrsL-.jpg",
+						"url": "https://t.co/ekOF9d3C8Y",
+						"display_url": "pic.twitter.com/ekOF9d3C8Y",
+						"expanded_url": "https://twitter.com/SnazzyQ/status/1412083263925981185/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1125,
+								"h": 1525,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 885,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 502,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1412083235954126800,
+						"id_str": "1412083235954126858",
+						"indices": [
+							76,
+							99
+						],
+						"media_url": "http://pbs.twimg.com/media/E5i6OLhUcAokal1.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/E5i6OLhUcAokal1.jpg",
+						"url": "https://t.co/ekOF9d3C8Y",
+						"display_url": "pic.twitter.com/ekOF9d3C8Y",
+						"expanded_url": "https://twitter.com/SnazzyQ/status/1412083263925981185/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 854,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 484,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1125,
+								"h": 1581,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1412083236184805400,
+						"id_str": "1412083236184805378",
+						"indices": [
+							76,
+							99
+						],
+						"media_url": "http://pbs.twimg.com/media/E5i6OMYUUAIT1dU.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/E5i6OMYUUAIT1dU.jpg",
+						"url": "https://t.co/ekOF9d3C8Y",
+						"display_url": "pic.twitter.com/ekOF9d3C8Y",
+						"expanded_url": "https://twitter.com/SnazzyQ/status/1412083263925981185/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1125,
+								"h": 1360,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 993,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 563,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1412083236407181300,
+						"id_str": "1412083236407181314",
+						"indices": [
+							76,
+							99
+						],
+						"media_url": "http://pbs.twimg.com/media/E5i6ONNVgAIHFdt.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/E5i6ONNVgAIHFdt.jpg",
+						"url": "https://t.co/ekOF9d3C8Y",
+						"display_url": "pic.twitter.com/ekOF9d3C8Y",
+						"expanded_url": "https://twitter.com/SnazzyQ/status/1412083263925981185/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 525,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 926,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1125,
+								"h": 1458,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 18055351,
+				"id_str": "18055351",
+				"name": "Quinn Nelson",
+				"screen_name": "SnazzyQ",
+				"location": "Salt Lake City, Utah",
+				"description": "i make videos about computers and stuff",
+				"url": "https://t.co/EDeqc5nziy",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/EDeqc5nziy",
+								"expanded_url": "http://youtube.com/snazzy",
+								"display_url": "youtube.com/snazzy",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 116459,
+				"friends_count": 273,
+				"listed_count": 836,
+				"created_at": "Thu Dec 11 18:17:50 +0000 2008",
+				"favourites_count": 23435,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 111123,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "ABB8C2",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme5/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme5/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1322418289360789504/8Kk4LkcX_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1322418289360789504/8Kk4LkcX_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/18055351/1480452794",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "99CC33",
+				"profile_text_color": "3E4415",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 7,
+			"favorite_count": 387,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "it",
+		"text": "Un po’ voglio il tagliere con scritto Alice ADSL. https://t.co/trPQJjSiG0"
+	},
+	{
 		"created_at": "Sun Jul 04 20:54:04 +0000 2021",
 		"id": 1411790437573181400,
 		"id_str": "1411790437573181447",
