fef8a33a4d0cda561260b0e23a875d3e6ac53b64
darksideofg <42369239+darksideofg@users.noreply.github.com>
2021-07-21 05:20:48 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26302,
-	"created_at": "Tue Jul 20 03:20:47 +0000 2021",
+	"tweets": 26303,
+	"created_at": "Wed Jul 21 03:20:47 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Tue Jul 20 13:50:11 +0000 2021",
+		"id": 1417481133026070500,
+		"id_str": "1417481970393702408",
+		"full_text": "a lot of space discourse today but not a single mention of the most important space trip in history https://t.co/UWSULYzYeh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			99
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1417481131428139000,
+					"id_str": "1417481131428139020",
+					"indices": [
+						100,
+						123
+					],
+					"media_url": "http://pbs.twimg.com/media/E6vnlDKX0AwUhEx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E6vnlDKX0AwUhEx.jpg",
+					"url": "https://t.co/UWSULYzYeh",
+					"display_url": "pic.twitter.com/UWSULYzYeh",
+					"expanded_url": "https://twitter.com/rymfarr/status/1417481133026070542/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 414,
+							"h": 233,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 414,
+							"h": 233,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 414,
+							"h": 233,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1417481131428139000,
+					"id_str": "1417481131428139020",
+					"indices": [
+						100,
+						123
+					],
+					"media_url": "http://pbs.twimg.com/media/E6vnlDKX0AwUhEx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E6vnlDKX0AwUhEx.jpg",
+					"url": "https://t.co/UWSULYzYeh",
+					"display_url": "pic.twitter.com/UWSULYzYeh",
+					"expanded_url": "https://twitter.com/rymfarr/status/1417481133026070542/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 414,
+							"h": 233,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 414,
+							"h": 233,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 414,
+							"h": 233,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1155639948894265300,
+			"id_str": "1155639948894265345",
+			"name": "ryan farr",
+			"screen_name": "rymfarr",
+			"location": "",
+			"description": "Stella Artois enthusiast (he/him)",
+			"url": "https://t.co/81qlgSmcZ5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/81qlgSmcZ5",
+							"expanded_url": "http://instagram.com/ryfarr",
+							"display_url": "instagram.com/ryfarr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8216,
+			"friends_count": 596,
+			"listed_count": 2,
+			"created_at": "Mon Jul 29 00:43:29 +0000 2019",
+			"favourites_count": 42644,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6042,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1390467236754857984/NnXO_zCz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1390467236754857984/NnXO_zCz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1155639948894265345/1587169041",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 398,
+		"favorite_count": 1740,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "a lot of space discourse today but not a single mention of the most important space trip in history https://t.co/UWSULYzYeh"
+	},
+	{
 		"created_at": "Mon Jul 19 18:49:12 +0000 2021",
 		"id": 1417194831403753500,
 		"id_str": "1417194831403753475",
