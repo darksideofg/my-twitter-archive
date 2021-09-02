99d525b06425e02a8b74261ce7f151b725663e21
darksideofg <42369239+darksideofg@users.noreply.github.com>
2021-09-02 05:20:48 +0200

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26353,
-	"created_at": "Wed Sep 01 03:20:47 +0000 2021",
+	"tweets": 26356,
+	"created_at": "Thu Sep 02 03:20:47 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_09.js
@@ -0,0 +1,605 @@
+Grailbird.data.tweets_2021_09 = 
+[
+	{
+		"created_at": "Wed Sep 01 21:34:14 +0000 2021",
+		"id": 1433181427751923700,
+		"id_str": "1433181427751923714",
+		"full_text": "Imagine being dragged by Peppa Pig https://t.co/D2cASGXBxs",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			34
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/D2cASGXBxs",
+					"expanded_url": "https://twitter.com/samir/status/1433115364175532035",
+					"display_url": "twitter.com/samir/status/1…",
+					"indices": [
+						35,
+						58
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 535277787,
+			"id_str": "535277787",
+			"name": "G (̄・・̄)",
+			"screen_name": "DarkSideOfG",
+			"location": "εᴍ - Siberia",
+			"description": "“The drought was the very worst / When the flowers that we'd grown together died of thirst”",
+			"url": "https://t.co/kzM9iE3UtX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kzM9iE3UtX",
+							"expanded_url": "http://darksideofg.tk",
+							"display_url": "darksideofg.tk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 106,
+			"friends_count": 77,
+			"listed_count": 6,
+			"created_at": "Sat Mar 24 09:46:29 +0000 2012",
+			"favourites_count": 7667,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 26063,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "515151",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_link_color": "686868",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "595959",
+			"profile_text_color": "606060",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1433115364175532000,
+		"quoted_status_id_str": "1433115364175532035",
+		"quoted_status_permalink": {
+			"url": "https://t.co/D2cASGXBxs",
+			"expanded": "https://twitter.com/samir/status/1433115364175532035",
+			"display": "twitter.com/samir/status/1…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Sep 01 17:11:43 +0000 2021",
+			"id": 1433115364175532000,
+			"id_str": "1433115364175532035",
+			"full_text": "The peppa pig tweet is gone, but we’ll never forget it https://t.co/gLz3qBnZGM",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				54
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1433115360165830700,
+						"id_str": "1433115360165830657",
+						"indices": [
+							55,
+							78
+						],
+						"media_url": "http://pbs.twimg.com/media/E-Ny07zXIAEBy0R.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/E-Ny07zXIAEBy0R.jpg",
+						"url": "https://t.co/gLz3qBnZGM",
+						"display_url": "pic.twitter.com/gLz3qBnZGM",
+						"expanded_url": "https://twitter.com/samir/status/1433115364175532035/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 314,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 555,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 947,
+								"h": 2048,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1433115360165830700,
+						"id_str": "1433115360165830657",
+						"indices": [
+							55,
+							78
+						],
+						"media_url": "http://pbs.twimg.com/media/E-Ny07zXIAEBy0R.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/E-Ny07zXIAEBy0R.jpg",
+						"url": "https://t.co/gLz3qBnZGM",
+						"display_url": "pic.twitter.com/gLz3qBnZGM",
+						"expanded_url": "https://twitter.com/samir/status/1433115364175532035/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 314,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 555,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 947,
+								"h": 2048,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 115396965,
+				"id_str": "115396965",
+				"name": "#1 samir",
+				"screen_name": "samir",
+				"location": "New York City",
+				"description": "I'm an account on the internet",
+				"url": "https://t.co/slc0QP0D9f",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/slc0QP0D9f",
+								"expanded_url": "https://kale-salad.lnk.to/a-a-a-a-a-very-good-songTW",
+								"display_url": "kale-salad.lnk.to/a-a-a-a-a-very…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 28369,
+				"friends_count": 2982,
+				"listed_count": 588,
+				"created_at": "Thu Feb 18 14:43:07 +0000 2010",
+				"favourites_count": 11049,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 19605,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFFFF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/791645957431648256/1rzMcwxX_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/791645957431648256/1rzMcwxX_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/115396965/1423024529",
+				"profile_link_color": "000000",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 198,
+			"favorite_count": 1188,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Imagine being dragged by Peppa Pig https://t.co/D2cASGXBxs"
+	},
+	{
+		"created_at": "Wed Sep 01 21:27:53 +0000 2021",
+		"id": 1433178430292496400,
+		"id_str": "1433179831118745613",
+		"full_text": "\"where do you see yourself in five years\" https://t.co/OlZDuBTdVY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			41
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/OlZDuBTdVY",
+					"expanded_url": "https://twitter.com/kyliebytes/status/1433151901852258304",
+					"display_url": "twitter.com/kyliebytes/sta…",
+					"indices": [
+						42,
+						65
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 824743172873998300,
+			"id_str": "824743172873998336",
+			"name": "Emma Twersky",
+			"screen_name": "twerske",
+			"location": "sf",
+			"description": "rhubarb enthusiast & DevRel Engineer @ Google, she/her",
+			"url": "https://t.co/fgbiMGgC9b",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fgbiMGgC9b",
+							"expanded_url": "http://twerske.dev",
+							"display_url": "twerske.dev",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1759,
+			"friends_count": 219,
+			"listed_count": 31,
+			"created_at": "Thu Jan 26 22:17:50 +0000 2017",
+			"favourites_count": 4676,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 843,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1393280515877183493/BHZJfnrF_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1393280515877183493/BHZJfnrF_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/824743172873998336/1586810945",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1433151901852258300,
+		"quoted_status_id_str": "1433151901852258304",
+		"quoted_status_permalink": {
+			"url": "https://t.co/OlZDuBTdVY",
+			"expanded": "https://twitter.com/kyliebytes/status/1433151901852258304",
+			"display": "twitter.com/kyliebytes/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Sep 01 19:36:54 +0000 2021",
+			"id": 1433151901852258300,
+			"id_str": "1433151901852258304",
+			"full_text": "Melissa Ong left a job she hated at Google in January 2020 after burning out under intense pressure. She opted to focus instead on her comedy career on TikTok, where she now has 3.8 million followers. https://t.co/xYUUXFjnfK",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				224
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/xYUUXFjnfK",
+						"expanded_url": "https://www.businessinsider.com/melissa-ong-google-tech-pursue-career-comedy-2021-8",
+						"display_url": "businessinsider.com/melissa-ong-go…",
+						"indices": [
+							201,
+							224
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 253350725,
+				"id_str": "253350725",
+				"name": "Kylie Robison",
+				"screen_name": "kyliebytes",
+				"location": "San Francisco, CA",
+				"description": "Covering dev communities, culture, tools, et al. @BITech ☆ Robison ≠ Robinson ☆ Send tips krobison@insider.com or DM :)",
+				"url": "https://t.co/GzXhQqGEHG",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/GzXhQqGEHG",
+								"expanded_url": "https://linktr.ee/kylierobison",
+								"display_url": "linktr.ee/kylierobison",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 13752,
+				"friends_count": 992,
+				"listed_count": 124,
+				"created_at": "Thu Feb 17 02:10:05 +0000 2011",
+				"favourites_count": 127119,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 15160,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFFFF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1430268600934289409/D6syOTYD_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1430268600934289409/D6syOTYD_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/253350725/1630445967",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "CF87CF",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 11,
+			"favorite_count": 61,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 4,
+		"favorite_count": 17,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"where do you see yourself in five years\" https://t.co/OlZDuBTdVY"
+	},
+	{
+		"created_at": "Wed Sep 01 14:37:02 +0000 2021",
+		"id": 1433076438316552200,
+		"id_str": "1433076438316552195",
+		"full_text": "Is the fourth season of “The Sinner” the one in which Harry finally goes to therapy?",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			84
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 535277787,
+			"id_str": "535277787",
+			"name": "G (̄・・̄)",
+			"screen_name": "DarkSideOfG",
+			"location": "εᴍ - Siberia",
+			"description": "“The drought was the very worst / When the flowers that we'd grown together died of thirst”",
+			"url": "https://t.co/kzM9iE3UtX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kzM9iE3UtX",
+							"expanded_url": "http://darksideofg.tk",
+							"display_url": "darksideofg.tk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 106,
+			"friends_count": 77,
+			"listed_count": 6,
+			"created_at": "Sat Mar 24 09:46:29 +0000 2012",
+			"favourites_count": 7667,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 26063,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "515151",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_link_color": "686868",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "595959",
+			"profile_text_color": "606060",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "Is the fourth season of “The Sinner” the one in which Harry finally goes to therapy?"
+	}
+]
