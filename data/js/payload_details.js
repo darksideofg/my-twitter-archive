7fc9307abaa31aaef05efadccdb53d5e5ec8151e
darksideofg <42369239+darksideofg@users.noreply.github.com>
2021-10-06 05:20:48 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26372,
-	"created_at": "Tue Oct 05 03:20:47 +0000 2021",
+	"tweets": 26373,
+	"created_at": "Wed Oct 06 03:20:47 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Tue Oct 05 20:23:20 +0000 2021",
+		"id": 1445462161384378400,
+		"id_str": "1445484773485338631",
+		"full_text": "That’s right https://t.co/Ux1tWC5Zdw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			12
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1445462151817154600,
+					"id_str": "1445462151817154582",
+					"indices": [
+						13,
+						36
+					],
+					"media_url": "http://pbs.twimg.com/media/FA9QKy9VUBYO0VP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FA9QKy9VUBYO0VP.jpg",
+					"url": "https://t.co/Ux1tWC5Zdw",
+					"display_url": "pic.twitter.com/Ux1tWC5Zdw",
+					"expanded_url": "https://twitter.com/caseyjohnston/status/1445462161384378376/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1445462151817154600,
+					"id_str": "1445462151817154582",
+					"indices": [
+						13,
+						36
+					],
+					"media_url": "http://pbs.twimg.com/media/FA9QKy9VUBYO0VP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FA9QKy9VUBYO0VP.jpg",
+					"url": "https://t.co/Ux1tWC5Zdw",
+					"display_url": "pic.twitter.com/Ux1tWC5Zdw",
+					"expanded_url": "https://twitter.com/caseyjohnston/status/1445462161384378376/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1445461750011154400,
+		"in_reply_to_status_id_str": "1445461750011154436",
+		"in_reply_to_user_id": 16472748,
+		"in_reply_to_user_id_str": "16472748",
+		"in_reply_to_screen_name": "caseyjohnston",
+		"user": {
+			"id": 16472748,
+			"id_str": "16472748",
+			"name": "Casey Johnston",
+			"screen_name": "caseyjohnston",
+			"location": "Brooklyn, NY",
+			"description": "free agent, baby / get in touch: casey@caseyjohnston.net / instagram: swolewoman",
+			"url": "https://t.co/xtBAu2EXMA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xtBAu2EXMA",
+							"expanded_url": "https://shesabeast.substack.com/about",
+							"display_url": "shesabeast.substack.com/about",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 48441,
+			"friends_count": 877,
+			"listed_count": 1130,
+			"created_at": "Fri Sep 26 19:27:15 +0000 2008",
+			"favourites_count": 40175,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 36251,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A161D",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1338127761249013762/hGPAUxKM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1338127761249013762/hGPAUxKM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16472748/1450387441",
+			"profile_link_color": "696969",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "4A0154",
+			"profile_text_color": "828282",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 11,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "That’s right https://t.co/Ux1tWC5Zdw"
+	},
+	{
 		"created_at": "Mon Oct 04 18:35:47 +0000 2021",
 		"id": 1445095321126768600,
 		"id_str": "1445095321126768647",
