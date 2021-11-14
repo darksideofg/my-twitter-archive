3faac932cc6bcd3b76a5b5eff5ee71fa4d791cba
darksideofg <42369239+darksideofg@users.noreply.github.com>
2021-11-14 04:20:47 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26395,
-	"created_at": "Sat Nov 13 03:20:47 +0000 2021",
+	"tweets": 26396,
+	"created_at": "Sun Nov 14 03:20:47 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,10 +1,116 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Sat Nov 13 06:52:00 +0000 2021",
+		"id": 1459413723567775700,
+		"id_str": "1459413723567775750",
+		"full_text": "Morale: quando ridate le cose al vostro ex non tenetevi mai nulla. 🧣 \n\nhttps://t.co/X9ZjHzRFtN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			94
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/X9ZjHzRFtN",
+					"expanded_url": "https://youtu.be/tollGa3S0o8",
+					"display_url": "youtu.be/tollGa3S0o8",
+					"indices": [
+						71,
+						94
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 535277787,
+			"id_str": "535277787",
+			"name": "G (̄・・̄)",
+			"screen_name": "DarkSideOfG",
+			"location": "εᴍ - Siberia",
+			"description": "“The drought was the very worst / When the flowers that we'd grown together died of thirst”",
+			"url": "https://t.co/kzM9iE3UtX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kzM9iE3UtX",
+							"expanded_url": "http://darksideofg.tk",
+							"display_url": "darksideofg.tk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 105,
+			"friends_count": 77,
+			"listed_count": 6,
+			"created_at": "Sat Mar 24 09:46:29 +0000 2012",
+			"favourites_count": 7790,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 26112,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "515151",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_link_color": "686868",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "595959",
+			"profile_text_color": "606060",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "it",
+		"text": "Morale: quando ridate le cose al vostro ex non tenetevi mai nulla. 🧣 \n\nhttps://t.co/X9ZjHzRFtN"
+	},
+	{
 		"created_at": "Fri Nov 12 14:22:38 +0000 2021",
 		"id": 1459164739360202800,
 		"id_str": "1459164739360202757",
-		"full_text": "Alzino la mano le bimbe di “Message in a Bottle” 🙋🏻‍♂️ #RedTaylorsVersion",
+		"full_text": "Alzino la mano le bimbe di “Message in a Bottle” 🙋🏻♂️ #RedTaylorsVersion",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -101,7 +207,7 @@ Grailbird.data.tweets_2021_11 =
 		"favorited": false,
 		"retweeted": false,
 		"lang": "it",
-		"text": "Alzino la mano le bimbe di “Message in a Bottle” 🙋🏻‍♂️ #RedTaylorsVersion"
+		"text": "Alzino la mano le bimbe di “Message in a Bottle” 🙋🏻♂️ #RedTaylorsVersion"
 	},
 	{
 		"created_at": "Thu Nov 11 15:57:26 +0000 2021",
