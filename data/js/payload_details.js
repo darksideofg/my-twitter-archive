c4d50b7d8d6304738516d3ba1df194ab3b19d0ee
darksideofg <42369239+darksideofg@users.noreply.github.com>
2022-01-04 04:20:47 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26432,
-	"created_at": "Sun Jan 02 03:20:47 +0000 2022",
+	"tweets": 26433,
+	"created_at": "Tue Jan 04 03:20:47 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,89 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Mon Jan 03 23:08:16 +0000 2022",
+		"id": 1478100346425675800,
+		"id_str": "1478141189459173382",
+		"full_text": "Sometimes I get frustrated when I compare my success to other people and then I remember that they are smarter, more talented, and harder working and then I feel better.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			169
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 61592079,
+			"id_str": "61592079",
+			"name": "Hank Green",
+			"screen_name": "hankgreen",
+			"location": "Missoula, MT",
+			"description": "Internet Person, Science Communicator, Author, etc.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1098769,
+			"friends_count": 831,
+			"listed_count": 5492,
+			"created_at": "Thu Jul 30 20:36:34 +0000 2009",
+			"favourites_count": 45884,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 45423,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "4DB3C8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1467381266534125569/AGEuh9UZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1467381266534125569/AGEuh9UZ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/61592079/1638591353",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "858585",
+			"profile_sidebar_fill_color": "C0DDE0",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 619,
+		"favorite_count": 8072,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Sometimes I get frustrated when I compare my success to other people and then I remember that they are smarter, more talented, and harder working and then I feel better."
+	},
+	{
 		"created_at": "Sat Jan 01 10:03:53 +0000 2022",
 		"id": 1477133216834330600,
 		"id_str": "1477219014405509120",
@@ -103,10 +186,10 @@ Grailbird.data.tweets_2022_01 =
 		"user": {
 			"id": 1947072912,
 			"id_str": "1947072912",
-			"name": "KonstantinBoristovich🐮🛠🧝🏼‍♀️🐗👜",
+			"name": "KonstantinBoristovich🐮🛠🧝🏼♀️🐗👜",
 			"screen_name": "HardSciFiMovies",
 			"location": "",
-			"description": "𝑰’𝒎 𝑻𝒘𝒊𝒕𝒕𝒆𝒓 𝑪𝒆𝒍𝒆𝒃𝒓𝒊𝒕𝒚 “𝑲𝒐𝒏𝒔𝒕𝒂𝒏𝒕𝒊𝒏𝑩𝒐𝒓𝒊𝒔𝒕𝒐𝒗𝒊𝒄𝒉🐮🛠🧝🏼‍♀️🐗👜”",
+			"description": "𝑰’𝒎 𝑻𝒘𝒊𝒕𝒕𝒆𝒓 𝑪𝒆𝒍𝒆𝒃𝒓𝒊𝒕𝒚 “𝑲𝒐𝒏𝒔𝒕𝒂𝒏𝒕𝒊𝒏𝑩𝒐𝒓𝒊𝒔𝒕𝒐𝒗𝒊𝒄𝒉🐮🛠🧝🏼♀️🐗👜”",
 			"url": null,
 			"entities": {
 				"description": {
