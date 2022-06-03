31af4e3533b8b2bcb9b70036ab8b1d727e801234
darksideofg <42369239+darksideofg@users.noreply.github.com>
2022-06-03 05:16:04 +0200

    Added 1 tweets

--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,293 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Thu Jun 02 04:30:35 +0000 2022",
+		"id": 1532218094420115500,
+		"id_str": "1532218094420115456",
+		"full_text": "Legends https://t.co/4IxE7c9AiR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			7
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/4IxE7c9AiR",
+					"expanded_url": "https://twitter.com/MsRebeccaBlack/status/1532168765697187841",
+					"display_url": "twitter.com/MsRebeccaBlack…",
+					"indices": [
+						8,
+						31
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 535277787,
+			"id_str": "535277787",
+			"name": "G (̄・・̄)",
+			"screen_name": "DarkSideOfG",
+			"location": "εᴍ - Siberia",
+			"description": "“The drought was the very worst / When the flowers that we'd grown together died of thirst”",
+			"url": "https://t.co/kzM9iE3UtX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kzM9iE3UtX",
+							"expanded_url": "http://darksideofg.tk",
+							"display_url": "darksideofg.tk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 106,
+			"friends_count": 79,
+			"listed_count": 6,
+			"created_at": "Sat Mar 24 09:46:29 +0000 2012",
+			"favourites_count": 8110,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 26310,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "515151",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_link_color": "686868",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "595959",
+			"profile_text_color": "606060",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1532168765697187800,
+		"quoted_status_id_str": "1532168765697187841",
+		"quoted_status_permalink": {
+			"url": "https://t.co/4IxE7c9AiR",
+			"expanded": "https://twitter.com/MsRebeccaBlack/status/1532168765697187841",
+			"display": "twitter.com/MsRebeccaBlack…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Jun 02 01:14:34 +0000 2022",
+			"id": 1532168765697187800,
+			"id_str": "1532168765697187841",
+			"full_text": "happy pride month https://t.co/wbMjroTWvn",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				17
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1532168752627732500,
+						"id_str": "1532168752627732480",
+						"indices": [
+							18,
+							41
+						],
+						"media_url": "http://pbs.twimg.com/media/FUNbXNxUUAAJj8X.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FUNbXNxUUAAJj8X.jpg",
+						"url": "https://t.co/wbMjroTWvn",
+						"display_url": "pic.twitter.com/wbMjroTWvn",
+						"expanded_url": "https://twitter.com/MsRebeccaBlack/status/1532168765697187841/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1024,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 1024,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1532168752627732500,
+						"id_str": "1532168752627732480",
+						"indices": [
+							18,
+							41
+						],
+						"media_url": "http://pbs.twimg.com/media/FUNbXNxUUAAJj8X.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FUNbXNxUUAAJj8X.jpg",
+						"url": "https://t.co/wbMjroTWvn",
+						"display_url": "pic.twitter.com/wbMjroTWvn",
+						"expanded_url": "https://twitter.com/MsRebeccaBlack/status/1532168765697187841/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1024,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 1024,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 268479992,
+				"id_str": "268479992",
+				"name": "Rebecca Black",
+				"screen_name": "MsRebeccaBlack",
+				"location": "",
+				"description": "READ MY MIND feat SLAYYYTER OUT NOW // booking: RebeccaBlackTeam@unitedtalent.com press: jordan@frazescreative.com\nshe/her",
+				"url": "https://t.co/u3bZyv7Fqg",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/u3bZyv7Fqg",
+								"expanded_url": "http://li.sten.to/ReadMyMind",
+								"display_url": "li.sten.to/ReadMyMind",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1114012,
+				"friends_count": 10889,
+				"listed_count": 4187,
+				"created_at": "Fri Mar 18 21:29:23 +0000 2011",
+				"favourites_count": 13844,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 17435,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": true,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme10/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme10/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1512181994926473243/ORSYj3YG_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1512181994926473243/ORSYj3YG_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/268479992/1639265834",
+				"profile_link_color": "F58EA8",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "FFBEB2",
+				"profile_text_color": "FF0000",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2967,
+			"favorite_count": 127394,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Legends https://t.co/4IxE7c9AiR"
+	},
+	{
 		"created_at": "Wed Jun 01 09:26:12 +0000 2022",
 		"id": 1531930099125207000,
 		"id_str": "1531930099125207040",
