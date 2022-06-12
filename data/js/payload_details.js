b4505659b827846fd55ffa8adcba7b32074232e9
darksideofg <42369239+darksideofg@users.noreply.github.com>
2022-06-12 05:16:04 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26579,
-	"created_at": "Sat Jun 11 03:16:03 +0000 2022",
+	"tweets": 26580,
+	"created_at": "Sun Jun 12 03:16:03 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Sat Jun 11 19:27:14 +0000 2022",
+		"id": 1535705235078930400,
+		"id_str": "1535705235078930433",
+		"full_text": "@gianlucawho Prima di aprire la foto ho scambiato Rea per una mucca. 🙈 ma non glielo dire che se no poi mi tiene il muso https://t.co/IEVsaXSDvm",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			120
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "gianlucawho",
+					"name": "gianlucasbagliato",
+					"id": 216920539,
+					"id_str": "216920539",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1535705229819318300,
+					"id_str": "1535705229819318272",
+					"indices": [
+						121,
+						144
+					],
+					"media_url": "http://pbs.twimg.com/media/FU_rxR8XEAArT_Y.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FU_rxR8XEAArT_Y.jpg",
+					"url": "https://t.co/IEVsaXSDvm",
+					"display_url": "pic.twitter.com/IEVsaXSDvm",
+					"expanded_url": "https://twitter.com/DarkSideOfG/status/1535705235078930433/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 610,
+							"h": 339,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 610,
+							"h": 339,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 610,
+							"h": 339,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1535705229819318300,
+					"id_str": "1535705229819318272",
+					"indices": [
+						121,
+						144
+					],
+					"media_url": "http://pbs.twimg.com/media/FU_rxR8XEAArT_Y.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FU_rxR8XEAArT_Y.jpg",
+					"url": "https://t.co/IEVsaXSDvm",
+					"display_url": "pic.twitter.com/IEVsaXSDvm",
+					"expanded_url": "https://twitter.com/DarkSideOfG/status/1535705235078930433/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 610,
+							"h": 339,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 610,
+							"h": 339,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 610,
+							"h": 339,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1535701119220994000,
+		"in_reply_to_status_id_str": "1535701119220994048",
+		"in_reply_to_user_id": 216920539,
+		"in_reply_to_user_id_str": "216920539",
+		"in_reply_to_screen_name": "gianlucawho",
+		"user": {
+			"id": 535277787,
+			"id_str": "535277787",
+			"name": "G (̄・・̄)",
+			"screen_name": "DarkSideOfG",
+			"location": "εᴍ - Siberia",
+			"description": "“The drought was the very worst / When the flowers that we'd grown together died of thirst”",
+			"url": "https://t.co/kzM9iE3UtX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kzM9iE3UtX",
+							"expanded_url": "http://darksideofg.tk",
+							"display_url": "darksideofg.tk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 106,
+			"friends_count": 80,
+			"listed_count": 6,
+			"created_at": "Sat Mar 24 09:46:29 +0000 2012",
+			"favourites_count": 8121,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 26331,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "515151",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_link_color": "686868",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "595959",
+			"profile_text_color": "606060",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "it",
+		"text": "@gianlucawho Prima di aprire la foto ho scambiato Rea per una mucca. 🙈 ma non glielo dire che se no poi mi tiene il muso https://t.co/IEVsaXSDvm"
+	},
+	{
 		"created_at": "Fri Jun 10 22:44:59 +0000 2022",
 		"id": 1535392612600926200,
 		"id_str": "1535392612600926209",
