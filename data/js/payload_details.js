795060d91d4fbf6540cd4fada4ef93065bc82f51
darksideofg <42369239+darksideofg@users.noreply.github.com>
2022-08-02 05:16:04 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26601,
-	"created_at": "Fri Jul 29 03:16:03 +0000 2022",
+	"tweets": 26602,
+	"created_at": "Tue Aug 02 03:16:03 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_08.js
@@ -0,0 +1,86 @@
+Grailbird.data.tweets_2022_08 = 
+[
+	{
+		"created_at": "Mon Aug 01 06:32:55 +0000 2022",
+		"id": 1553869951127019500,
+		"id_str": "1553992152975347712",
+		"full_text": "I have no hobby or vacation or ambition for my life except computer",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			67
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2436389418,
+			"id_str": "2436389418",
+			"name": "SwiftOnSecurity",
+			"screen_name": "SwiftOnSecurity",
+			"location": "Cyber, USA",
+			"description": "Security person at a place. former helpdesk. they/them/tay. Microsoft MVP, Client Security",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 365241,
+			"friends_count": 9253,
+			"listed_count": 7015,
+			"created_at": "Thu Apr 10 02:54:26 +0000 2014",
+			"favourites_count": 130187,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 149798,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1540821244295221248/C7BeysCj_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1540821244295221248/C7BeysCj_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2436389418/1658252078",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 90,
+		"favorite_count": 620,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "I have no hobby or vacation or ambition for my life except computer"
+	}
+]
