a41ba5a72a05fbe695608533ae6fcd185c8bf605
darksideofg <42369239+darksideofg@users.noreply.github.com>
2022-08-27 05:16:04 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26614,
-	"created_at": "Wed Aug 24 03:16:04 +0000 2022",
+	"tweets": 26615,
+	"created_at": "Sat Aug 27 03:16:03 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,199 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Fri Aug 26 06:41:28 +0000 2022",
+		"id": 1562823957559931000,
+		"id_str": "1563053998793695233",
+		"full_text": "Momma Sea Otter Making Sure Her Puppy is Comfy https://t.co/wFKHG5fpjQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			46
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1562823797186732000,
+					"id_str": "1562823797186732032",
+					"indices": [
+						47,
+						70
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1562823797186732032/pu/img/zWQgW59iXol8R3HG.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1562823797186732032/pu/img/zWQgW59iXol8R3HG.jpg",
+					"url": "https://t.co/wFKHG5fpjQ",
+					"display_url": "pic.twitter.com/wFKHG5fpjQ",
+					"expanded_url": "https://twitter.com/Gabriele_Corno/status/1562823957559930880/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 800,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1562823797186732000,
+					"id_str": "1562823797186732032",
+					"indices": [
+						47,
+						70
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1562823797186732032/pu/img/zWQgW59iXol8R3HG.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1562823797186732032/pu/img/zWQgW59iXol8R3HG.jpg",
+					"url": "https://t.co/wFKHG5fpjQ",
+					"display_url": "pic.twitter.com/wFKHG5fpjQ",
+					"expanded_url": "https://twitter.com/Gabriele_Corno/status/1562823957559930880/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 800,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 800,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							4,
+							5
+						],
+						"duration_millis": 33875,
+						"variants": [
+							{
+								"bitrate": 950000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1562823797186732032/pu/vid/480x600/FLGdU-84qoIzAEib.mp4?tag=12"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1562823797186732032/pu/vid/320x400/i0YreuusRfEcohh5.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1562823797186732032/pu/pl/opFf7Xh-SIQlDFh5.m3u8?tag=12&container=fmp4&v=6ee"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1562823797186732032/pu/vid/640x800/-XfqG5XkrsWPpqsZ.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 374752807,
+			"id_str": "374752807",
+			"name": "Gabriele Corno",
+			"screen_name": "Gabriele_Corno",
+			"location": "Montreux, Suisse",
+			"description": "Life is like Alice's White Rabbit. He runs away and does not grab anything, but this is not important: what matters is to keep on pursuing him. Personal Account",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 171240,
+			"friends_count": 2694,
+			"listed_count": 1775,
+			"created_at": "Fri Sep 16 21:44:30 +0000 2011",
+			"favourites_count": 82483,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 73129,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "0099B9",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/557958031606939649/jExOWkdO_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/557958031606939649/jExOWkdO_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/374752807/1599323956",
+			"profile_link_color": "0099B9",
+			"profile_sidebar_border_color": "5ED4DC",
+			"profile_sidebar_fill_color": "95E8EC",
+			"profile_text_color": "3C3940",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12981,
+		"favorite_count": 105907,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Momma Sea Otter Making Sure Her Puppy is Comfy https://t.co/wFKHG5fpjQ"
+	},
+	{
 		"created_at": "Tue Aug 23 07:59:08 +0000 2022",
 		"id": 1561986382406492200,
 		"id_str": "1561986382406492160",
