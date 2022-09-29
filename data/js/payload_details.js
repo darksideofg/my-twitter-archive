bdd229b03d421819392e0aa2075fae0ce095cae4
darksideofg <42369239+darksideofg@users.noreply.github.com>
2022-09-29 05:16:04 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26639,
-	"created_at": "Tue Sep 27 03:16:03 +0000 2022",
+	"tweets": 26640,
+	"created_at": "Thu Sep 29 03:16:03 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,102 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Wed Sep 28 16:14:45 +0000 2022",
+		"id": 1575102818293551000,
+		"id_str": "1575157072806699009",
+		"full_text": "“i hate that i understand this reference”  you don’t hate it.  you love being terminally online enough to understand every esoteric discourse that exists.  just admit it",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			169
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1093741515430596600,
+			"id_str": "1093741515430596608",
+			"name": "transgender marx",
+			"screen_name": "JUNlPER",
+			"location": "she/they",
+			"description": "juniper - an evil transsexual and a reputable journalist @NYTimes posting thru the brain rot",
+			"url": "https://t.co/h2vLraJQW5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/h2vLraJQW5",
+							"expanded_url": "https://boxd.it/3Vmj7",
+							"display_url": "boxd.it/3Vmj7",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 102290,
+			"friends_count": 2229,
+			"listed_count": 173,
+			"created_at": "Fri Feb 08 05:21:12 +0000 2019",
+			"favourites_count": 304431,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 156195,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1573409281189330958/RCYniwrp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1573409281189330958/RCYniwrp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1093741515430596608/1653355847",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1109,
+		"favorite_count": 13841,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "“i hate that i understand this reference”  you don’t hate it.  you love being terminally online enough to understand every esoteric discourse that exists.  just admit it"
+	},
+	{
 		"created_at": "Mon Sep 26 06:42:26 +0000 2022",
 		"id": 1574288268438503400,
 		"id_str": "1574288268438503427",
