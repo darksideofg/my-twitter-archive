e1ae5df522b267fd88f975b10e0230646d75ae4f
darksideofg <42369239+darksideofg@users.noreply.github.com>
2022-12-06 04:16:04 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26694,
-	"created_at": "Sat Dec 03 03:16:03 +0000 2022",
+	"tweets": 26695,
+	"created_at": "Tue Dec 06 03:16:03 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_12.js
+++ data/js/tweets/2022_12.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2022_12 = 
 [
 	{
+		"created_at": "Mon Dec 05 08:40:56 +0000 2022",
+		"id": 1599129761405550600,
+		"id_str": "1599685240062181376",
+		"full_text": "what if we kissed… under the… https://t.co/zEkc4LAXcl",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			29
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1599129757186138000,
+					"id_str": "1599129757186138113",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/FjFACqbWIAEvky3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FjFACqbWIAEvky3.jpg",
+					"url": "https://t.co/zEkc4LAXcl",
+					"display_url": "pic.twitter.com/zEkc4LAXcl",
+					"expanded_url": "https://twitter.com/megelison/status/1599129761405550594/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1599129757186138000,
+					"id_str": "1599129757186138113",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/FjFACqbWIAEvky3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FjFACqbWIAEvky3.jpg",
+					"url": "https://t.co/zEkc4LAXcl",
+					"display_url": "pic.twitter.com/zEkc4LAXcl",
+					"expanded_url": "https://twitter.com/megelison/status/1599129761405550594/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16889723,
+			"id_str": "16889723",
+			"name": "Meg Elison",
+			"screen_name": "megelison",
+			"location": "Maryland, USA",
+			"description": "Author. P.K. Dick & Locus Award Winner. Hugo, Nebula, Sturgeon & Otherwise finalist. Nonfic editor @UncannyMagazine. https://t.co/HBK23vMF3n she/they/sir 🏳️‍🌈",
+			"url": "https://t.co/HBK23vMF3n",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/HBK23vMF3n",
+							"expanded_url": "http://megelison.com",
+							"display_url": "megelison.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/HBK23vMF3n",
+							"expanded_url": "http://megelison.com",
+							"display_url": "megelison.com",
+							"indices": [
+								117,
+								140
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 16684,
+			"friends_count": 1508,
+			"listed_count": 239,
+			"created_at": "Tue Oct 21 17:27:50 +0000 2008",
+			"favourites_count": 172169,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 81342,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1596905811124649984/4s37mq-6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1596905811124649984/4s37mq-6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16889723/1661826178",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 684,
+		"favorite_count": 5185,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "what if we kissed… under the… https://t.co/zEkc4LAXcl"
+	},
+	{
 		"created_at": "Fri Dec 02 23:39:33 +0000 2022",
 		"id": 1598559406068994000,
 		"id_str": "1598824218736615424",
