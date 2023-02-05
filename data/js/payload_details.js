9517ddb405467a2923012980a550bd85868447ca
darksideofg <42369239+darksideofg@users.noreply.github.com>
2023-02-05 04:16:04 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26729,
-	"created_at": "Tue Jan 31 03:16:03 +0000 2023",
+	"tweets": 26730,
+	"created_at": "Sun Feb 05 03:16:03 +0000 2023",
 	"lang": "en"
 }
---
+++ data/js/tweets/2023_02.js
@@ -0,0 +1,290 @@
+Grailbird.data.tweets_2023_02 = 
+[
+	{
+		"created_at": "Sat Feb 04 11:04:58 +0000 2023",
+		"id": 1621827145113772000,
+		"id_str": "1621827145113772032",
+		"full_text": "Incredibilmente anche AOTY 2023 https://t.co/RpxpnOrtBX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			31
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/RpxpnOrtBX",
+					"expanded_url": "https://twitter.com/whereismuna/status/1621654194888544256",
+					"display_url": "twitter.com/whereismuna/st…",
+					"indices": [
+						32,
+						55
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 535277787,
+			"id_str": "535277787",
+			"name": "G (̄・・̄)",
+			"screen_name": "DarkSideOfG",
+			"location": "εᴍ - Siberia",
+			"description": "“The drought was the very worst / When the flowers that we'd grown together died of thirst”",
+			"url": "https://t.co/X8kvpF6nYq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/X8kvpF6nYq",
+							"expanded_url": "http://darksideofg.tk",
+							"display_url": "darksideofg.tk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 106,
+			"friends_count": 82,
+			"listed_count": 6,
+			"created_at": "Sat Mar 24 09:46:29 +0000 2012",
+			"favourites_count": 8397,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 26445,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "515151",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_link_color": "686868",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "595959",
+			"profile_text_color": "606060",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1621654194888544300,
+		"quoted_status_id_str": "1621654194888544256",
+		"quoted_status_permalink": {
+			"url": "https://t.co/RpxpnOrtBX",
+			"expanded": "https://twitter.com/whereismuna/status/1621654194888544256",
+			"display": "twitter.com/whereismuna/st…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Feb 03 23:37:43 +0000 2023",
+			"id": 1621654194888544300,
+			"id_str": "1621654194888544256",
+			"full_text": "happy 6th anni one of the most truly devastating, almost worryingly sad albums ever recorded https://t.co/O6g6iAc2n5",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				92
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1621654145907433500,
+						"id_str": "1621654145907433473",
+						"indices": [
+							93,
+							116
+						],
+						"media_url": "http://pbs.twimg.com/media/FoFF2pNaIAEmxjL.png",
+						"media_url_https": "https://pbs.twimg.com/media/FoFF2pNaIAEmxjL.png",
+						"url": "https://t.co/O6g6iAc2n5",
+						"display_url": "pic.twitter.com/O6g6iAc2n5",
+						"expanded_url": "https://twitter.com/whereisMUNA/status/1621654194888544256/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 900,
+								"h": 900,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 900,
+								"h": 900,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1621654145907433500,
+						"id_str": "1621654145907433473",
+						"indices": [
+							93,
+							116
+						],
+						"media_url": "http://pbs.twimg.com/media/FoFF2pNaIAEmxjL.png",
+						"media_url_https": "https://pbs.twimg.com/media/FoFF2pNaIAEmxjL.png",
+						"url": "https://t.co/O6g6iAc2n5",
+						"display_url": "pic.twitter.com/O6g6iAc2n5",
+						"expanded_url": "https://twitter.com/whereisMUNA/status/1621654194888544256/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 900,
+								"h": 900,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 900,
+								"h": 900,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2176972214,
+				"id_str": "2176972214",
+				"name": "MUNA",
+				"screen_name": "whereisMUNA",
+				"location": "",
+				"description": "greatest band in the world",
+				"url": "https://t.co/DxDqRg2W4e",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/DxDqRg2W4e",
+								"expanded_url": "http://whereismuna.com",
+								"display_url": "whereismuna.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 80467,
+				"friends_count": 296,
+				"listed_count": 221,
+				"created_at": "Tue Nov 05 23:05:21 +0000 2013",
+				"favourites_count": 19562,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 5017,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F3F3F3",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1503628499323535365/GnhAM117_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1503628499323535365/GnhAM117_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2176972214/1647327971",
+				"profile_link_color": "000000",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 837,
+			"favorite_count": 7545,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "it",
+		"text": "Incredibilmente anche AOTY 2023 https://t.co/RpxpnOrtBX"
+	}
+]
