34c9dfaadd61e3115a8f5535a8d9d9623bbb13ef
darksideofg <42369239+darksideofg@users.noreply.github.com>
2023-05-18 05:16:04 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 26772,
-	"created_at": "Tue May 16 03:16:03 +0000 2023",
+	"tweets": 26773,
+	"created_at": "Thu May 18 03:16:03 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_05.js
+++ data/js/tweets/2023_05.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2023_05 = 
 [
 	{
+		"created_at": "Wed May 17 09:33:36 +0000 2023",
+		"id": 1658767715996246000,
+		"id_str": "1658767715996246016",
+		"full_text": "@fedebruni84 https://t.co/9rSJBaxcw3",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			12
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "fedebruni84",
+					"name": "Federico 🇺🇦",
+					"id": 23112137,
+					"id_str": "23112137",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1658767712754053000,
+					"id_str": "1658767712754053122",
+					"indices": [
+						13,
+						36
+					],
+					"media_url": "http://pbs.twimg.com/media/FwUgcjuXsAIm3aO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FwUgcjuXsAIm3aO.jpg",
+					"url": "https://t.co/9rSJBaxcw3",
+					"display_url": "pic.twitter.com/9rSJBaxcw3",
+					"expanded_url": "https://twitter.com/DarkSideOfG/status/1658767715996246016/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 593,
+							"h": 446,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 593,
+							"h": 446,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 593,
+							"h": 446,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1658767712754053000,
+					"id_str": "1658767712754053122",
+					"indices": [
+						13,
+						36
+					],
+					"media_url": "http://pbs.twimg.com/media/FwUgcjuXsAIm3aO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FwUgcjuXsAIm3aO.jpg",
+					"url": "https://t.co/9rSJBaxcw3",
+					"display_url": "pic.twitter.com/9rSJBaxcw3",
+					"expanded_url": "https://twitter.com/DarkSideOfG/status/1658767715996246016/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 593,
+							"h": 446,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 593,
+							"h": 446,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 593,
+							"h": 446,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1658579494460899300,
+		"in_reply_to_status_id_str": "1658579494460899328",
+		"in_reply_to_user_id": 23112137,
+		"in_reply_to_user_id_str": "23112137",
+		"in_reply_to_screen_name": "fedebruni84",
+		"user": {
+			"id": 535277787,
+			"id_str": "535277787",
+			"name": "G (̄・・̄)",
+			"screen_name": "DarkSideOfG",
+			"location": "εᴍ - Siberia",
+			"description": "“The drought was the very worst / When the flowers that we'd grown together died of thirst”",
+			"url": "https://t.co/X8kvpF6nYq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/X8kvpF6nYq",
+							"expanded_url": "http://darksideofg.tk",
+							"display_url": "darksideofg.tk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 107,
+			"friends_count": 82,
+			"listed_count": 6,
+			"created_at": "Sat Mar 24 09:46:29 +0000 2012",
+			"favourites_count": 8559,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 26487,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "515151",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/708618466488684544/iC5GNINX_normal.jpg",
+			"profile_link_color": "686868",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "595959",
+			"profile_text_color": "606060",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "qme",
+		"text": "@fedebruni84 https://t.co/9rSJBaxcw3"
+	},
+	{
 		"created_at": "Mon May 15 15:34:07 +0000 2023",
 		"id": 1658133667896606700,
 		"id_str": "1658133667896606721",
